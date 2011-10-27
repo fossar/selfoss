f5af5b89802bcbb9da6cdb56c191b0226a48f390
Harald Lapp <harald.lapp@gmail.com>
2011-10-27 14:23:15 +0200

    initial commit backend classes (which indeed just use mysql backend)

---
+++ models/sqlite/Items.php
@@ -0,0 +1,15 @@
+<?PHP
+
+namespace models\sqlite;
+
+/**
+ * Class for accessing persistent saved items -- sqlite
+ *
+ * @package    models
+ * @copyright  Copyright (c) Harald Lapp <harald.lapp@gmail.com>
+ * @license    GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ * @author     Harald Lapp <harald.lapp@gmail.com>
+ */
+// class Items extends Database {
+class Items extends \models\mysql\Items {
+}
---
+++ models/sqlite/Sources.php
@@ -0,0 +1,14 @@
+<?PHP
+
+namespace models\sqlite;
+
+/**
+ * Class for accessing persistent saved sources -- mysql
+ *
+ * @package    models
+ * @copyright  Copyright (c) Harald Lapp <harald.lapp@gmail.com>
+ * @license    GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ * @author     Harald Lapp <harald.lapp@gmail.com>
+ */
+class Sources extends \models\mysql\Sources {
+}
