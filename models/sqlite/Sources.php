1aca06468343b1d3b8fffecaf4c3e0a2194a3da6
Chris Guidry <chris@theguidrys.us>
2011-10-29 13:06:43 +0200

    * sources will now be ordered by title correctly * clean separation of sqlite querys by redefine parents methods

--- models/sqlite/Sources.php
+++ models/sqlite/Sources.php
@@ -11,4 +11,40 @@ namespace models\sqlite;
  * @author     Harald Lapp <harald.lapp@gmail.com>
  */
 class Sources extends \models\mysql\Sources {
+
+    /**
+     * add new source
+     *
+     * @return int new id
+     * @param string $title
+     * @param string $spout the source type
+     * @param mixed $params depends from spout
+     */
+    public function add($title, $spout, $params) {
+        \DB::sql('INSERT INTO sources (title, spout, params) VALUES (:title, :spout, :params)',
+                    array(
+                        ':title'  => $title,
+                        ':spout'  => $spout,
+                        ':params' => htmlentities(json_encode($params))
+                    ));
+        
+        \DB::sql('SELECT last_insert_rowid() as lastid');
+        $res = \F3::get('DB->result');
+        return $res[0]['lastid'];
+    }
+
+    
+    /**
+     * returns all sources
+     *
+     * @return mixed all sources
+     */
+    public function get() {
+        \DB::sql('SELECT id, title, spout, params, error FROM sources ORDER BY lower(title) ASC');
+        $ret = \F3::get('DB->result');
+        $spoutLoader = new \helpers\SpoutLoader();
+        for($i=0;$i<count($ret);$i++)
+            $ret[$i]['spout_obj'] = $spoutLoader->get( $ret[$i]['spout'] );
+        return $ret;
+    }
 }
