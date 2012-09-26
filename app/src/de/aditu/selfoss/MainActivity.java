f22291b2d6b82c1e559bc7f3364ca5843d36914f
Chris Guidry <chris@theguidrys.us>
2012-09-26 19:56:47 +0200

    add complete android app project (phonegap based)

---
+++ app/src/de/aditu/selfoss/MainActivity.java
@@ -0,0 +1,16 @@
+package de.aditu.selfoss;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
