bb1114483967e8b4dddfc3dbe8a0c23799d5d72f
Michael Jackson <michael.o.jackson@gmail.com>
2013-03-14 22:12:21 -0400

    PostgreSQL support (initial revision)
    This adds support for PostgreSQL as a database for selfoss. Tested with
    pg 9.0, but anything reasonably modern (8.x+) should work.  Testing
    platform is debian stable on i386 with pg 9.0.7 and php 5.3.3-7.
    
    Squashed commit of the following:
    
    commit d65487baf61b60112772e4778f2f4f23082ad5a0
    Author: Michael Jackson <michael.o.jackson@gmail.com>
    Date:   Thu Mar 14 22:11:38 2013 -0400
    
        Fixing another boolean/integer mismatch with pg
    
    commit f325dfdb1fc02a3bf0e43d27dba52f0077a1018e
    Author: Michael Jackson <michael.o.jackson@gmail.com>
    Date:   Thu Mar 14 22:05:46 2013 -0400
    
        Further fixes for pg support
    
    commit 041050cc99188671889a599b86677980b1e45d1a
    Author: Michael Jackson <michael.o.jackson@gmail.com>
    Date:   Thu Mar 14 21:50:28 2013 -0400
    
        Initial revision of support for postgresql database.

---
+++ daos/pgsql/Database.php
@@ -0,0 +1,141 @@
+<?PHP
+
+namespace daos\pgsql;
+    
+/**
+ * Base class for database access -- postgresql
+ *
+ * Note that before use you'll want to create the database itself. See 
+ * http://www.postgresql.org/docs/8.4/static/manage-ag-createdb.html for full information.
+ * In a nutshell (from the command line), as the administrative user (postgres),
+ * execute "createdb -O USER DBNAME" where USER is the user you will be connecting as
+ * and DBNAME is the database to create. Administering users (roles) and authentication
+ * is out of scope for this comment, but the online postgresql documentation is comprehensive.
+ *
+ * @package     daos
+ * @copyright   Copyright (c) Michael Jackson <michael.o.jackson@gmail.com>
+ * @license     GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ * @author      Michael Jackson <michael.o.jackson@gmail.com>
+ * @author      Tobias Zeising <tobias.zeising@aditu.de>
+ */
+class Database {
+
+    /**
+     * indicates whether database connection was initialized
+     *
+     * @var bool
+     */
+    static private $initialized = false;
+
+    
+    /**
+     * establish connection and create undefined tables
+     *
+     * @return  void
+     */
+    public function __construct() {
+        if (self::$initialized === false && \F3::get('db_type')=="pgsql") {
+            // establish database connection
+            \F3::set('db', new \DB\SQL(
+                'pgsql:host=' . \F3::get('db_host') . ';port=' . \F3::get('db_port') . ';dbname='.\F3::get('db_database'),
+                \F3::get('db_username'),
+                \F3::get('db_password')
+            ));
+            
+            // create tables if necessary
+            $result = @\F3::get('db')->exec("SELECT table_name FROM information_schema.tables WHERE table_schema='public'");
+            $tables = array();
+            foreach($result as $table)
+                foreach($table as $key=>$value)
+                    $tables[] = $value;
+            
+            if(!in_array('items', $tables)) {
+                \F3::get('db')->exec('
+                    CREATE TABLE items (
+                        id          SERIAL PRIMARY KEY,
+                        datetime    TIMESTAMP WITH TIME ZONE NOT NULL,
+                        title       TEXT NOT NULL,
+                        content     TEXT NOT NULL,
+                        thumbnail   TEXT,
+                        icon        TEXT,
+                        unread      BOOLEAN NOT NULL,
+                        starred     BOOLEAN NOT NULL,
+                        source      INTEGER NOT NULL,
+                        uid         TEXT NOT NULL,
+                        link        TEXT NOT NULL
+                    );
+                ');
+                
+                \F3::get('db')->exec('
+                    CREATE INDEX source ON items (
+                        source
+                    );
+                ');
+            }
+            
+            $isNewestSourcesTable = false;
+            if(!in_array('sources', $tables)) {
+                \F3::get('db')->exec('
+                    CREATE TABLE sources (
+                        id          SERIAL PRIMARY KEY,
+                        title       TEXT NOT NULL,
+                        tags        TEXT,
+                        spout       TEXT NOT NULL,
+                        params      TEXT NOT NULL,
+                        error       TEXT 
+                    );
+                ');
+                $isNewestSourcesTable = true;
+            }
+                 
+            // version 1
+            if(!in_array('version', $tables)) {
+                \F3::get('db')->exec('
+                    CREATE TABLE version (
+                        version INTEGER
+                    );
+                ');
+                
+                \F3::get('db')->exec('
+                    INSERT INTO version (version) VALUES (2);
+                ');
+                
+                \F3::get('db')->exec('
+                    CREATE TABLE tags (
+                        tag         TEXT NOT NULL,
+                        color       TEXT NOT NULL
+                    );
+                ');
+                
+                if($isNewestSourcesTable===false) {
+                    \F3::get('db')->exec('
+                        ALTER TABLE sources ADD COLUMN tags TEXT;
+                    ');
+                }
+            }
+            
+            // just initialize once
+            $initialized = true;
+        }
+    }
+    
+    
+    /**
+     * optimize database by the database's own optimize statement
+     *
+     * Note that for pg, for full optimization you'd run "vacuum full analyze {table}".  This does require
+     * an exclusive lock on the table though and so this is probably best run offline during scheduled
+     * downtime.  See http://www.postgresql.org/docs/8.4/static/sql-vacuum.html for more information
+     * (particularly the notes in the footer of that page leading to further DBA-related info e.g. the
+     * autovacuum daemon).
+     *
+     * @return  void
+     */
+    public function optimize() {
+        $result = @\F3::get('db')->exec("SELECT table_name FROM information_schema.tables WHERE table_schema='public'");
+        $tables = array();
+        foreach($result as $table)
+            foreach($table as $key=>$value)
+                @\F3::get('db')->exec("VACUUM ANALYZE :table", array(':table' => $value));
+    }
+}
