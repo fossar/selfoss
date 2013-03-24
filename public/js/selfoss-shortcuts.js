0a18581bf8ad3cdb7a23f1a9407e5a7ca8c834ba
Brosseau Valentin <c4software@gmail.com>
2013-03-24 18:13:40 +0100

    Add swipe navigation

--- public/js/selfoss-shortcuts.js
+++ public/js/selfoss-shortcuts.js
@@ -48,6 +48,31 @@ selfoss.shortcuts = {
             selfoss.shortcuts.nextprev('prev', true);
             return false;
         });
+        
+        // Enable Swipe navigation for mobile and tablet
+        if(selfoss.isSmartphone() || selfoss.isTablet()){
+                // Event for swipe.
+                // Swipe left go to the next item in the list and mark as read
+                Hammer("body").on("swipeleft", function() {
+                    $('.entry.selected.unread .entry-unread').click();
+                    selfoss.shortcuts.nextprev('next', true);
+                    var fullscreen = $('#fullscreen-entry');
+                    fullscreen.html('<div id="entrr'+$(".selected").attr('id').substr(5)+'" class="entry fullscreen">'+$(".selected").html()+'</div>');
+                    selfoss.events.entriesToolbar(fullscreen);
+                    fullscreen.find('.entry-tags-tag').colorByBrightness();
+                });
+        
+                // Swipe right go to the previous item in the list and mark as read
+                Hammer("body").on("swiperight", function() {
+                    $('.entry.selected.unread .entry-unread').click();
+                    selfoss.shortcuts.nextprev('prev', true);
+                    var fullscreen = $('#fullscreen-entry');
+                    fullscreen.html('<div id="entrr'+$(".selected").attr('id').substr(5)+'" class="entry fullscreen">'+$(".selected").html()+'</div>');
+                    selfoss.events.entriesToolbar(fullscreen);
+                    fullscreen.find('.entry-tags-tag').colorByBrightness();
+                });
+        };
+        
     },
     
     
