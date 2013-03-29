b26c426c882fdbda8ae6cf393c91345a621b1f11
PowerIVQ <PowerIV.Q@gmail.com>
2013-03-28 19:24:25 -0500

    Update selfoss-shortcuts.js

--- public/js/selfoss-shortcuts.js
+++ public/js/selfoss-shortcuts.js
@@ -8,10 +8,12 @@ selfoss.shortcuts = {
         // next
         $(document).bind('keydown', 'space', function() { selfoss.shortcuts.nextprev('next', true, false); return false; });
         $(document).bind('keydown', 'n', function() { selfoss.shortcuts.nextprev('next', false); return false; });
+        $(document).bind('keydown', 'j', function() { selfoss.shortcuts.nextprev('next', true); return false; });
         
         // prev
         $(document).bind('keydown', 'shift+space', function() { selfoss.shortcuts.nextprev('prev', true); return false; });
         $(document).bind('keydown', 'p', function() { selfoss.shortcuts.nextprev('prev', false); return false; });
+        $(document).bind('keydown', 'k', function() { selfoss.shortcuts.nextprev('prev', true); return false; });
         
         // star/unstar
         $(document).bind('keydown', 's', function() {
@@ -39,19 +41,19 @@ selfoss.shortcuts = {
         });
         
         // mark all as read
-        $(document).bind('keydown', 'shift+a', function() {
+        $(document).bind('keydown', 'ctrl+m', function() {
             $('#nav-mark').click();
         });
 
         // throw (mark as read & open next)
-        $(document).bind('keydown', 'j', function() {
+        $(document).bind('keydown', 't', function() {
             $('.entry.selected.unread .entry-unread').click();
             selfoss.shortcuts.nextprev('next', true);
             return false;
         });
 
         // throw (mark as read & open previous)
-        $(document).bind('keydown', 'k', function() {
+        $(document).bind('keydown', 'Shift+t', function() {
             $('.entry.selected.unread .entry-unread').click();
             selfoss.shortcuts.nextprev('prev', true);
             return false;
