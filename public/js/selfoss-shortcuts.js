13e6b5958bfad25ee6b604f6b4974252d6665bc1
Eric <eric@eric-op-het.net>
2013-03-30 13:02:35 +0100

    Navigate feeds using your arrow keys
    left key = previous
    right key = next

--- public/js/selfoss-shortcuts.js
+++ public/js/selfoss-shortcuts.js
@@ -8,11 +8,13 @@ selfoss.shortcuts = {
         // next
         $(document).bind('keydown', 'space', function() { selfoss.shortcuts.nextprev('next', true, false); return false; });
         $(document).bind('keydown', 'n', function() { selfoss.shortcuts.nextprev('next', false); return false; });
+        $(document).bind('keydown', 'right', function() { selfoss.shortcuts.nextprev('next', false); return false; });
         $(document).bind('keydown', 'j', function() { selfoss.shortcuts.nextprev('next', true); return false; });
         
         // prev
         $(document).bind('keydown', 'shift+space', function() { selfoss.shortcuts.nextprev('prev', true); return false; });
         $(document).bind('keydown', 'p', function() { selfoss.shortcuts.nextprev('prev', false); return false; });
+        $(document).bind('keydown', 'left', function() { selfoss.shortcuts.nextprev('prev', false); return false; });
         $(document).bind('keydown', 'k', function() { selfoss.shortcuts.nextprev('prev', true); return false; });
         
         // star/unstar
