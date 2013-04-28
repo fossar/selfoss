fossar/selfoss

commit 15ffa09fe413c842760d6d0f685a2be8c37bac46

Clean up orphaned items (fixes #291)
Extends the cleanup function of the items daos to remove any items
whose source field doesn't have a corresponding entry in the sources
table.

The argument is now optional, so if cleanup is called without
arguments, it will only remove orphaned items from the database.
Behaviour of cleanup when called with an argument remains unchanged.

daos/mysql/Items.php
@@ -144,13 +144,17 @@ class Items extends Database {
     
     
     /**
-     * cleanup old items
+     * cleanup orphaned and old items
      *
      * @return void
-     * @param DateTime $date date to delete all items older than this value
+     * @param DateTime $date date to delete all items older than this value [optional]
      */
-    public function cleanup(\DateTime $date) {
-        \F3::get('db')->exec('DELETE FROM items WHERE starred=0 AND datetime<:date',
+    public function cleanup(\DateTime $date = NULL) {
+        \F3::get('db')->exec('DELETE FROM items WHERE id IN (
+                                SELECT items.id FROM items LEFT JOIN sources
+                                ON items.source=sources.id WHERE sources.id IS NULL)');
+        if ($date !== NULL)
+            \F3::get('db')->exec('DELETE FROM items WHERE starred=0 AND datetime<:date',
                     array(':date' => $date->format('Y-m-d').' 00:00:00'));
     }
     

daos/Items.php
@@ -48,15 +48,17 @@ class Items extends Database {
     
     
     /**
-     * cleanup old items
+     * cleanup orphaned and old items
      *
      * @return void
-     * @param int $days delete all items older than this value
+     * @param int $days delete all items older than this value [optional]
      */
     public function cleanup($days) {
-        $minDate = new \DateTime();
-        $minDate->sub(new \DateInterval('P'.$days.'D'));
-        
+        $minDate = NULL;
+        if ($days !== "") {
+            $minDate = new \DateTime();
+            $minDate->sub(new \DateInterval('P'.$days.'D'));
+        }
         $this->backend->cleanup($minDate);
     }
     

daos/pgsql/Items.php
@@ -198,13 +198,17 @@ class Items extends \daos\mysql\Items {
     }
 
     /**
-     * cleanup old items
+     * cleanup orphaned and old items
      *
      * @return void
-     * @param DateTime $date date to delete all items older than this value
+     * @param DateTime $date date to delete all items older than this value [optional]
      */
-    public function cleanup(\DateTime $date) {
-        \F3::get('db')->exec('DELETE FROM items WHERE starred=false AND datetime<:date',
+    public function cleanup(\DateTime $date = NULL) {
+        \F3::get('db')->exec('DELETE FROM items WHERE id IN (
+                                SELECT items.id FROM items LEFT JOIN sources
+                                ON items.source=sources.id WHERE sources.id IS NULL)');
+        if ($date !== NULL)
+            \F3::get('db')->exec('DELETE FROM items WHERE starred=false AND datetime<:date',
                     array(':date' => $date->format('Y-m-d').' 00:00:00'));
     }
 }

helpers/ContentLoader.php
@@ -197,13 +197,11 @@ class ContentLoader {
      * @return void
      */
     public function cleanup() {
-        // cleanup old items
-        if(\F3::get('items_lifetime')) {
-            \F3::get('logger')->log('cleanup old items', \DEBUG);
-            $itemsDao = new \daos\Items();
-            $itemsDao->cleanup(\F3::get('items_lifetime'));
-            \F3::get('logger')->log('cleanup old items finished', \DEBUG);
-        }
+        // cleanup orphaned and old items
+        \F3::get('logger')->log('cleanup orphaned and old items', \DEBUG);
+        $itemsDao = new \daos\Items();
+        $itemsDao->cleanup(\F3::get('items_lifetime'));
+        \F3::get('logger')->log('cleanup orphaned and old items finished', \DEBUG);
         
         // delete orphaned thumbnails
         \F3::get('logger')->log('delete orphaned thumbnails', \DEBUG);

commit bcf9fc7cc15977cc7fda9c363d20ac7f58cc2938

Fix update.php failing with MySQL
MySQL doesn't support the DELETE syntax introduced in commit 15ffa09.
This fixes #298.

daos/mysql/Items.php
@@ -150,9 +150,8 @@ class Items extends Database {
      * @param DateTime $date date to delete all items older than this value [optional]
      */
     public function cleanup(\DateTime $date = NULL) {
-        \F3::get('db')->exec('DELETE FROM items WHERE id IN (
-                                SELECT items.id FROM items LEFT JOIN sources
-                                ON items.source=sources.id WHERE sources.id IS NULL)');
+        \F3::get('db')->exec('DELETE FROM items USING items LEFT JOIN sources
+                                ON items.source=sources.id WHERE sources.id IS NULL');
         if ($date !== NULL)
             \F3::get('db')->exec('DELETE FROM items WHERE starred=0 AND datetime<:date',
                     array(':date' => $date->format('Y-m-d').' 00:00:00'));

daos/sqlite/Items.php
@@ -13,4 +13,20 @@ namespace daos\sqlite;
  */
 // class Items extends Database {
 class Items extends \daos\mysql\Items {
+
+    /**
+     * cleanup orphaned and old items
+     *
+     * @return void
+     * @param DateTime $date date to delete all items older than this value [optional]
+     */
+    public function cleanup(\DateTime $date = NULL) {
+        \F3::get('db')->exec('DELETE FROM items WHERE id IN (
+                                SELECT items.id FROM items LEFT JOIN sources
+                                ON items.source=sources.id WHERE sources.id IS NULL)');
+        if ($date !== NULL)
+            \F3::get('db')->exec('DELETE FROM items WHERE starred=0 AND datetime<:date',
+                    array(':date' => $date->format('Y-m-d').' 00:00:00'));
+    }
+
 }