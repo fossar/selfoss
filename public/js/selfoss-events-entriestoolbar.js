c30b43981ea3adea275c5a5a03be6f1df5c2cde2
Chris Guidry <chris@theguidrys.us>
2014-09-28 11:47:32 -0400

    Switching open links with a normal <a target=_blank> instead of using window.open.

--- public/js/selfoss-events-entriestoolbar.js
+++ public/js/selfoss-events-entriestoolbar.js
@@ -4,56 +4,49 @@
 selfoss.events.entriesToolbar = function(parent) {
     if(typeof parent == "undefined")
         parent = $('#content');
-    
+
     // prevent close on links
     parent.find('a').unbind('click').click(function(e) {
         window.open($(this).attr('href'));
         e.preventDefault();
         return false;
     });
-    
+
     // load images
     parent.find('.entry-loadimages').unbind('click').click(function() {
         $(this).parents('.entry').lazyLoadImages();
         $(this).fadeOut();
         return false;
     });
-    
-    // open in new window
-    parent.find('.entry-newwindow').unbind('click').click(function(e) {
-        window.open($(this).parents(".entry").children(".entry-source").attr("href"));
-        e.preventDefault();
-        return false;
-    });
-    
+
     // share with google plus
     parent.find('.entry-sharegoogle').unbind('click').click(function(e) {
         window.open("https://plus.google.com/share?url="+encodeURIComponent($(this).parents(".entry").children(".entry-link").eq(0).attr("href")));
         e.preventDefault();
         return false;
     });
-    
+
     // share with twitter
     parent.find('.entry-sharetwitter').unbind('click').click(function(e) {
         window.open("https://twitter.com/intent/tweet?source=webclient&text="+encodeURIComponent($(this).parents(".entry").children(".entry-title").html())+" "+encodeURIComponent($(this).parents(".entry").children(".entry-link").eq(0).attr("href")));
         e.preventDefault();
         return false;
     });
-    
+
     // share with facebook
     parent.find('.entry-sharefacebook').unbind('click').click(function(e) {
         window.open("https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent($(this).parents(".entry").children(".entry-link").eq(0).attr("href"))+"&t="+encodeURIComponent($(this).parents(".entry").children(".entry-title").html()));
         e.preventDefault();
         return false;
     });
-    
+
     // share with pocket
     parent.find('.entry-sharepocket').unbind('click').click(function(e) {
         window.open(" https://getpocket.com/save?url="+encodeURIComponent($(this).parents(".entry").children(".entry-link").eq(0).attr("href"))+"&title="+encodeURIComponent($(this).parents(".entry").children(".entry-title").html()));
         e.preventDefault();
         return false;
     });
-  
+
     // share with delicious
     parent.find('.entry-sharedelicious').unbind('click').click(function(e) {
         var href = $(this).parents(".entry").children(".entry-link").eq(0).attr("href");
@@ -83,7 +76,7 @@ selfoss.events.entriesToolbar = function(parent) {
         e.preventDefault();
         return false;
     });
-    
+
     // only loggedin users
     if($('body').hasClass('loggedin')==true) {
         // starr/unstarr
@@ -92,7 +85,7 @@ selfoss.events.entriesToolbar = function(parent) {
             var id = parent.attr('id').substr(5);
             var starr = $(this).hasClass('active')==false;
             var button = $("#entry"+id+" .entry-starr, #entrr"+id+" .entry-starr");
-            
+
             // update button
             var setButton = function(starr) {
                 if(starr) {
@@ -104,7 +97,7 @@ selfoss.events.entriesToolbar = function(parent) {
                 }
             };
             setButton(starr);
-            
+
             // update statistics in main menue
             var updateStats = function(starr) {
                 var starred = parseInt($('.nav-filter-starred span').html());
@@ -116,7 +109,7 @@ selfoss.events.entriesToolbar = function(parent) {
                 $('.nav-filter-starred span').html(starred);
             };
             updateStats(starr);
-            
+
             $.ajax({
                 url: $('base').attr('href') + (starr ? 'starr/' : 'unstarr/') + id,
                 type: 'POST',
@@ -124,13 +117,13 @@ selfoss.events.entriesToolbar = function(parent) {
                     // rollback ui changes
                     setButton(!starr);
                     updateStats(!starr);
-                    selfoss.showError('Can not star/unstar item: '+errorThrown); 
+                    selfoss.showError('Can not star/unstar item: '+errorThrown);
                 }
             });
-            
+
             return false;
         });
-        
+
         // read/unread
         parent.find('.entry-unread').unbind('click').click(function() {
             var id = $(this).parents('.entry').attr('id').substr(5);
@@ -151,7 +144,7 @@ selfoss.events.entriesToolbar = function(parent) {
                 }
             };
             setButton(unread);
-            
+
             // update statistics in main menue and the currently active tag
             var updateStats = function(unread) {
                 // update all unread counter
@@ -165,7 +158,7 @@ selfoss.events.entriesToolbar = function(parent) {
                 $('.nav-filter-unread span').removeClass('unread');
                 if(unreadstats>0)
                     $('.nav-filter-unread span').addClass('unread');
-                    
+
                 // update unread count on sources
                 var sourceId = $('#entry'+id+' .entry-source').attr('class').substr(25);
                 var sourceNav = $('#source'+sourceId+' .unread');
@@ -180,34 +173,34 @@ selfoss.events.entriesToolbar = function(parent) {
                     $('#source'+sourceId+'').addClass('unread');
                 }
                 sourceNav.html(sourceCount);
-                
+
                 // update unread on tags
                 $('#entry'+id+' .entry-tags-tag').each( function(index) {
                     var tag = $(this).html();
-                    
+
                     var tagsCountEl = $('#nav-tags > li > span.tag').filter(function(i){
                         return $(this).html()==tag; }
                     ).next();
-                    
+
                     var unreadstats = 0;
                     if (tagsCountEl.html()!='')
                         unreadstats = parseInt(tagsCountEl.html());
-                    
+
                     if (unread)
                         unreadstats--;
                     else
                         unreadstats++;
-                    
+
                     if (unreadstats>0)
                         tagsCountEl.html(unreadstats);
                     else
                         tagsCountEl.html('');
-                    
+
                 } );
                 selfoss.setUnreadCount(unreadstats);
             };
             updateStats(unread);
-            
+
             $.ajax({
                 url: $('base').attr('href') + (unread ? 'mark/' : 'unmark/') + id,
                 data: { ajax: true },
@@ -216,10 +209,10 @@ selfoss.events.entriesToolbar = function(parent) {
                     // rollback ui changes
                     updateStats(!unread);
                     setButton(!unread);
-                    selfoss.showError('Can not mark/unmark item: '+errorThrown); 
+                    selfoss.showError('Can not mark/unmark item: '+errorThrown);
                 }
             });
-            
+
             return false;
         });
     }
