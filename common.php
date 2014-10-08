8d37fff1f72c9f3e05a7c31cf26d139562a9a1af
root <root@mailer.auzias.net>
2014-10-08 16:40:45 +0200

    within common.php set f3->('ASSETS', ..) with the full URL of the assets. Some use-cases with apache2/nginx proposes client resource through http instead of https...

--- common.php
+++ common.php
@@ -8,6 +8,17 @@ $f3->set('AUTOLOAD',__dir__.'/;libs/f3/;libs/;libs/WideImage/;daos/;libs/twitter
 $f3->set('cache',__dir__.'/data/cache');
 $f3->set('BASEDIR',__dir__);
 $f3->set('LOCALES',__dir__.'/public/lang/');
+//If the website is loaded through HTTPS we set the full URL (which does not natively work when using apache2 as back-end and nginx as front-end).
+$assets = "http";
+  if( (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) && $_SERVER['HTTP_X_FORWARDED_PROTO']=="https" ||
+      (isset($_SERVER['HTTP_HTTPS'])) && $_SERVER['HTTP_HTTPS']=="https")
+  {
+    $assets .= "s";
+  } else {
+    $assets .= "";
+  }
+  $assets .= "://".$_SERVER['HTTP_HOST'].'/';
+$f3->set('ASSETS', $assets);
 
 // read defaults
 $f3->config('defaults.ini');
