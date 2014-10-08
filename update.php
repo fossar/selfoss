20dddc3cd91c0142326c9c1b24570a38c90258ac
Tobias Zeising <tobias.zeising@aditu.de>
2014-10-08 09:17:23 +0200

    add fulltextrss configs

--- update.php
+++ update.php
@@ -3,7 +3,7 @@
 chdir(__DIR__);
 require(__DIR__.'/common.php');
 
-$f3->set('FTRSS_DATA_DIR', __dir__.'/data/ftrss');
+$f3->set('FTRSS_DATA_DIR', __dir__.'/data/fulltextrss');
 
 $loader = new \helpers\ContentLoader();
 $loader->update();
