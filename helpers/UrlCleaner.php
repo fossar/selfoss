60655792e53f0ac4bde48d471e7396a5a6fcde8b
Jean Baptiste Favre <github@jbfavre.org>
2015-01-07 19:11:53 +0100

    Code cleaning

--- helpers/UrlCleaner.php
+++ helpers/UrlCleaner.php
@@ -4,7 +4,8 @@ namespace helpers;
 
 /**
  * Helper class for getting real URL from feed
- * as well as removing tracking params
+ * as well as removing tracking params from
+ * query string or fragments
  *
  * @package    helpers
  * @copyright  Copyright (c) Jean Baptiste Favre (http://www.jbfavre.org)
@@ -19,6 +20,24 @@ class UrlCleaner {
      */
     private $realUrl = false;
 
+    /**
+     * patterns to look for in URL host
+     * @var array
+     */
+    private $hostPatterns = ['feeds', 'rss', 't.co'];
+
+    /**
+     * patterns to look for in URL query string
+     * @var array
+     */
+    private $queryPatterns = ['utm_', 'xtor'];
+
+    /**
+     * patterns to look for in URL fragment
+     * @var array
+     */
+    private $fragmentPatterns = ['xtor', 'ens_id'];
+
 
     /**
      * get real url if specific patterns are found in host
@@ -28,20 +47,22 @@ class UrlCleaner {
      */
     public function processUrl($url) {
 	$urlToken = parse_url($url);
-	$patterns = ['feeds', 'rss'];
-        foreach ( $patterns as $pattern ) {
-            if ( strpos( $urlToken['host'], $pattern) !== false ) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL,$url);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-                curl_setopt($ch, CURLOPT_HEADER, true);
-                curl_exec($ch);
-                $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-                break;
-            }
-        }
-        $this->realUrl = $this->cleanTrackers($url);
+
+	// Look for host patterns. If found, use curl to get real URL
+        foreach ( $this->hostPatterns as $pattern ) {
+	    if ( strpos($urlToken['host'], $pattern) !== false ) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, TRUE);
+		curl_exec($ch);
+		$htis->realUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+		unset($ch);
+		break;
+	    }
+	}
+	$this->realUrl = $this->cleanTrackers($this->realUrl);
     }
 
     /**
@@ -60,30 +81,40 @@ class UrlCleaner {
      */
     private function cleanTrackers() {
         $url = parse_url($this->realUrl);
-        // Next, rebuild URL
+
+	// Start rebuilding URL
         $realUrl = $url['scheme'] . '://';
         if (isset($url['user']) && isset($url['password']))
             $realUrl .= $url['user'] . ':' . $url['password'] . '@';
         $realUrl .= $url['host'] . $url['path'];
+
         // Query string
         if (isset($url['query'])) {
             parse_str($url['query'], $q_array);
             $realQuery = array();
             foreach ($q_array as $key => $value) {
-                // Remove utm_* &xtor parameters
-                if(strpos($key, 'utm_')===false && strpos($key, 'xtor')===false)
-                    $realQuery[]= $key.'='.$value;
+                // Remove trackers from URL query string
+		foreach ( $this->queryPatterns as $pattern ) {
+		    if ( strpos($key, 'utm_') === false && strpos($key, 'xtor') === false )
+			$realQuery[]= $key.'='.$value;
+		}
             }
-            if (count($real_query))
+            if ( count($real_query) )
                 $realUrl .= '?' . implode('&', $realQuery);
+	    unset($q_array);
+	    unset($realQuery);
         }
+
         // Fragment
         if (isset($url['fragment'])) {
-            // Remove xtor=RSS anchor
-            if (strpos($url['fragment'], 'xtor')===false)
-                $realUrl .= '#' . $url['fragment'];
+	    // Remove trackers from URL fragment
+	    foreach ( $this->fragmentPatterns as $pattern ) {
+		if ( strpos($url['fragment'], 'xtor') === false )
+		    $realUrl .= '#' . $url['fragment'];
+	    }
         }
         $this->realUrl = $realUrl;
+	unset($url);
     }
 
 }
