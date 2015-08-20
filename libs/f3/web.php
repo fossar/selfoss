869de025e9263aa07fcfa941687adc3a289fc835
Alexandre Rossi <alexandre.rossi@gmail.com>
2015-08-20 09:45:22 +0200

    f3/Web: workaround CURLOPT_FOLLOWLOCATION not allowed when open_basedir is set
    This allows the use of curl when open_basedir is set. Otherwise, the following
    error makes the request fail:
        Error curl_setopt() [function.curl-setopt]: CURLOPT_FOLLOWLOCATION cannot be
        activated when safe_mode is enabled or an open_basedir is set
    
    This was reported as SSilence/selfoss#691.
    
    Those changes are cherry-picked from upstream:
        https://github.com/bcosca/fatfree-core/commit/c05e146cb21f8903727af4c5dc0a0171c9f21530
        https://github.com/bcosca/fatfree-core/commit/125d783e965b7f932de520fc12349d5473ef93f4

--- libs/f3/web.php
+++ libs/f3/web.php
@@ -249,10 +249,13 @@ class Web extends Prefab {
 	**/
 	protected function _curl($url,$options) {
 		$curl=curl_init($url);
-		curl_setopt($curl,CURLOPT_FOLLOWLOCATION,
-			$options['follow_location']);
+		if (!ini_get('open_basedir'))
+			curl_setopt($curl,CURLOPT_FOLLOWLOCATION,
+				$options['follow_location']);
 		curl_setopt($curl,CURLOPT_MAXREDIRS,
 			$options['max_redirects']);
+		curl_setopt($curl,CURLOPT_PROTOCOLS,CURLPROTO_HTTP|CURLPROTO_HTTPS);
+		curl_setopt($curl,CURLOPT_REDIR_PROTOCOLS,CURLPROTO_HTTP|CURLPROTO_HTTPS);
 		curl_setopt($curl,CURLOPT_CUSTOMREQUEST,$options['method']);
 		if (isset($options['header']))
 			curl_setopt($curl,CURLOPT_HTTPHEADER,$options['header']);
@@ -278,6 +281,11 @@ class Web extends Prefab {
 		curl_exec($curl);
 		curl_close($curl);
 		$body=ob_get_clean();
+		if ($options['follow_location'] &&
+			preg_match('/^Location: (.+)$/m',implode(PHP_EOL,$headers),$loc)) {
+			$options['max_redirects']--;
+			return $this->request($loc[1],$options);
+		}
 		return array(
 			'body'=>$body,
 			'headers'=>$headers,
