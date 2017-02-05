3143d9fbbb18d773e69afccc60ae2c99e8d55833
Jan Tojnar <jtojnar@gmail.com>
2017-02-05 01:15:11 +0100

    Remove the vendor directory

--- vendor/smottt/wideimage/lib/WideImage/Exception/InvalidImageSourceException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace WideImage\Exception;
-
-/**
- * @package Exceptions
- */
-class InvalidImageSourceException extends Exception {}
--- vendor/smottt/wideimage/test/run.bat
+++
@@ -1,1 +0,0 @@
-phpunit --verbose --bootstrap test-init.php %1 %2 %3 %4 %5 %6 WideImage
