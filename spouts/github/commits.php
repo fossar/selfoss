fossar/selfoss

commit 3c9da8de68763c86c700496cda0c38b978d832ef

Fix some problems reported by PHPStan level 0

spouts/github/commits.php
@@ -68,6 +68,9 @@ class commits extends \spouts\spout {
     /** @var string global html url for the source */
     protected $htmlUrl = '';
 
+    /** @var string URL of the favicon */
+    protected $faviconUrl = '';
+
     //
     // Iterator Interface
     //
@@ -86,7 +89,7 @@ class commits extends \spouts\spout {
     /**
      * receive current item
      *
-     * @return SimplePie_Item current item
+     * @return \SimplePie_Item current item
      */
     public function current() {
         if ($this->items !== false) {
@@ -112,7 +115,7 @@ class commits extends \spouts\spout {
     /**
      * select next item
      *
-     * @return SimplePie_Item next item
+     * @return \SimplePie_Item next item
      */
     public function next() {
         if ($this->items !== false) {

controllers/Index.php
@@ -221,7 +221,7 @@ class Index extends BaseController {
     /**
      * load items
      *
-     * @return html with items
+     * @return string html with items
      */
     private function loadItems($options, $tags) {
         $tagColors = $this->convertTagsToAssocArray($tags);
@@ -261,7 +261,7 @@ class Index extends BaseController {
      *
      * @param array $tags
      *
-     * @return tag color array
+     * @return array tag color array
      */
     private function convertTagsToAssocArray($tags) {
         $assocTags = [];

controllers/Opml.php
@@ -2,6 +2,8 @@
 
 namespace controllers;
 
+use SimpleXMLElement;
+
 /**
  * OPML loading and exporting controller
  *
@@ -23,10 +25,19 @@ class Opml extends BaseController {
     /** @var \helpers\SpoutLoader */
     private $spoutLoader;
 
+    /** @var \XMLWriter */
+    private $writer;
+
+    /** @var \daos\Sources */
+    private $sourcesDao;
+
+    /** @var \daos\Tags */
+    private $tagsDao;
+
     public function __construct() {
         parent::__construct();
 
-        $this->spoutLoader = new \helpers\SpoutLoader($this);
+        $this->spoutLoader = new \helpers\SpoutLoader();
     }
 
     /**
@@ -97,13 +108,13 @@ class Opml extends BaseController {
     /**
      * Process a group of outlines
      *
-     * @param $xml (SimpleXML) A SimpleXML object with <outline> children
-     * @param $tags (Array) An array of tags for the current <outline>
+     * @param SimpleXMLElement $xml A SimpleXML object with <outline> children
+     * @param array $tags An array of tags for the current <outline>
      * @note Recursive
      * @note We use non-rss outline's text as tags
      * @note Reads outline elements from both the default and selfoss namespace
      */
-    private function processGroup($xml, $tags = []) {
+    private function processGroup(SimpleXMLElement $xml, array $tags = []) {
         $errors = [];
 
         $xml->registerXPathNamespace('selfoss', 'https://selfoss.aditu.de/');
@@ -143,12 +154,12 @@ class Opml extends BaseController {
     /**
      * Add new feed subscription
      *
-     * @param $xml xml feed entry for item
-     * @param $tags of the entry
+     * @param SimpleXMLElement $xml xml feed entry for item
+     * @param array $tags of the entry
      *
-     * @return true on success or item title on error
+     * @return bool true on success or item title on error
      */
-    private function addSubscription($xml, $tags) {
+    private function addSubscription(SimpleXMLElement $xml, array $tags) {
         // OPML Required attributes: text, xmlUrl, type
         // Optional attributes: title, htmlUrl, language, title, version
         // Selfoss namespaced attributes: spout, params
@@ -219,10 +230,10 @@ class Opml extends BaseController {
     /**
      * Generate an OPML outline element from a source
      *
-     * @param $source source
+     * @param array $source source
      * @note Uses the selfoss namespace to store information about spouts
      */
-    private function writeSource($source) {
+    private function writeSource(array $source) {
         // retrieve the feed url of the source
         $params = json_decode(html_entity_decode($source['params']), true);
         $feedUrl = $this->spoutLoader->get($source['spout'])->getXmlUrl($params);

controllers/Sources.php
@@ -85,7 +85,7 @@ class Sources extends BaseController {
      * return all Sources suitable for navigation panel
      * html
      *
-     * @return htmltext
+     * @return string htmltext
      */
     public function renderSources($sources) {
         $html = '';
@@ -105,7 +105,7 @@ class Sources extends BaseController {
      * for navigation panel
      * html
      *
-     * @return htmltext
+     * @return string htmltext
      */
     public function sourcesListAsString() {
         $sourcesDao = new \daos\Sources();

controllers/Tags.php
@@ -26,7 +26,7 @@ class Tags extends BaseController {
      * returns all tags
      * html
      *
-     * @return void
+     * @return string
      */
     public function tagsListAsString() {
         $tagsDao = new \daos\Tags();
@@ -38,7 +38,7 @@ class Tags extends BaseController {
      * returns all tags
      * html
      *
-     * @return void
+     * @return string
      */
     public function renderTags($tags) {
         $html = '';

daos/Items.php
@@ -31,7 +31,7 @@ class Items extends Database {
      * @param string $name name of the function
      * @param array $args arguments
      *
-     * @return methods return value
+     * @return mixed methods return value
      */
     public function __call($name, $args) {
         if (method_exists($this->backend, $name)) {

daos/Sources.php
@@ -32,7 +32,7 @@ class Sources extends Database {
      * @param string $name name of the function
      * @param array $args arguments
      *
-     * @return methods return value
+     * @return mixed methods return value
      */
     public function __call($name, $args) {
         if (method_exists($this->backend, $name)) {

daos/Tags.php
@@ -45,7 +45,7 @@ class Tags extends Database {
      * @param string $name name of the function
      * @param array $args arguments
      *
-     * @return methods return value
+     * @return mixed methods return value
      */
     public function __call($name, $args) {
         if (method_exists($this->backend, $name)) {

commit 02b4c7e49a273d9f9fc3022d62043ac0105e54ff

Fix favicon for GitHub spout
3c9da8de68763c86c700496cda0c38b978d832ef broke obtaining the favicon.

Since the icon is always the same, this patch fixes it by hardcoding
the URL, saving a roundtrip to GitHub servers.

spouts/github/commits.php
@@ -69,7 +69,7 @@ class commits extends \spouts\spout {
     protected $htmlUrl = '';
 
     /** @var string URL of the favicon */
-    protected $faviconUrl = '';
+    protected $faviconUrl = 'https://assets-cdn.github.com/favicon.ico';
 
     //
     // Iterator Interface
@@ -216,17 +216,6 @@ class commits extends \spouts\spout {
      * @return string icon url
      */
     public function getIcon() {
-        if (isset($this->faviconUrl)) {
-            return $this->faviconUrl;
-        }
-
-        $this->faviconUrl = false;
-        $imageHelper = $this->getImageHelper();
-        $htmlUrl = $this->getHtmlUrl();
-        if ($htmlUrl && $imageHelper->fetchFavicon($htmlUrl)) {
-            $this->faviconUrl = $imageHelper->getFaviconUrl();
-        }
-
         return $this->faviconUrl;
     }
 