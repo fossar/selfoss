fossar/selfoss

commit f49e1ae11b3aa2dac50ebdf32e2dd278b8941cc8

allow width and height html attributes in feed content
This allows for earlier browser rendering when images are lazy loaded

helpers/ContentLoader.php
@@ -228,7 +228,7 @@ class ContentLoader {
             $content,
             array(
                 "safe"           => 1,
-                "deny_attribute" => '* -alt -title -src -href -target',
+                "deny_attribute" => '* -alt -title -src -href -target -width -height',
                 "keep_bad"       => 0,
                 "comment"        => 1,
                 "cdata"          => 1,

commit 5b5b217a2ed0125ad78b93a051dbded47408e6b0

sanitizer: allow width & height attributes for images
The width and height attributes were whitelisted in
f49e1ae11b3aa2dac50ebdf32e2dd278b8941cc8 which caused problems with
larger images since the max-width introduced in #544 does not
preserve aspect ratio. The attributes were then reverted for
img tag in 7bb268925698e724432be7c82eeaa384e7856297 but leaving it
for other elements like table, where it is actually unwanted.

This commit inverts that state since we want for websites to be able
to specify image dimensions and the aspect ratio is now respected.

helpers/ContentLoader.php
@@ -258,12 +258,13 @@ class ContentLoader {
             $content,
             [
                 'safe' => 1,
-                'deny_attribute' => '* -alt -title -src -href -target -width -height, img +width +height',
+                'deny_attribute' => '* -alt -title -src -href -target',
                 'keep_bad' => 0,
                 'comment' => 1,
                 'cdata' => 1,
                 'elements' => 'div,p,ul,li,a,img,dl,dt,dd,h1,h2,h3,h4,h5,h6,ol,br,table,tr,td,blockquote,pre,ins,del,th,thead,tbody,b,i,strong,em,tt,sub,sup,s,strike,code'
-            ]
+            ],
+            'img=width, height'
         );
     }
 