a3d2d3e5eb1814db19e63a6a9209a14b9ee8f372
Jan Tojnar <jtojnar@gmail.com>
2018-08-14 10:51:11 +0200

    ci: Skip cleanup for Bintray deploys
    Bintray deploys suddenly stopped working sometime between March 5 and March 9, 2018. Possibly, skip_cleanup was not implemented for Bintray deployer before that.

--- utils/bintray.sh
+++ utils/bintray.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+ls -al
 export SELFOSS_ZIPBALL=$(echo selfoss-*.zip)
 export SELFOSS_VERSION=$(jq -r '.ver' package.json)
+echo "------------------" $SELFOSS_VERSION $SELFOSS_ZIPBALL
 sed -i "s/SELFOSS_VERSION/$SELFOSS_VERSION/g;s/SELFOSS_ZIPBALL/$SELFOSS_ZIPBALL/g" utils/bintray.json
