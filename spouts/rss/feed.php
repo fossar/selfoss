c817b20b8e2239508dd1b144cee18ee443bbbd73
Jan Tojnar <jtojnar@gmail.com>
2018-09-20 03:26:42 +0200

    Do not add SimplePie into User-Agent
    For RSS Feeds, we have been adding “Simplepie/X.Y.Z” string to the User-Agent
    header. Given that this is just an implementation detail, and that many websites
    actively block SimplePie, we will no longer continue to do so.

--- spouts/rss/feed.php
+++ spouts/rss/feed.php
@@ -144,7 +144,7 @@ class feed extends \spouts\spout {
         @$this->feed->set_file_class('\helpers\SimplePieFileGuzzle');
         @$this->feed->set_feed_url(htmlspecialchars_decode($params['url']));
         @$this->feed->set_autodiscovery_level(SIMPLEPIE_LOCATOR_AUTODISCOVERY | SIMPLEPIE_LOCATOR_LOCAL_EXTENSION | SIMPLEPIE_LOCATOR_LOCAL_BODY);
-        $this->feed->set_useragent(\helpers\WebClient::getUserAgent(['SimplePie/' . SIMPLEPIE_VERSION]));
+        $this->feed->set_useragent(\helpers\WebClient::getUserAgent());
 
         // fetch items
         @$this->feed->init();
