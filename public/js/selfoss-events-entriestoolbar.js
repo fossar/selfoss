3ad1fdb9af9a0de65fa8928e8c8e0b2c519749b0
Akash Kakkar <akash.galaxy07@gmail.com>
2019-09-22 23:38:02 +0530

    a11y: Convert entry toolbars to lists

--- public/js/selfoss-events-entriestoolbar.js
+++ public/js/selfoss-events-entriestoolbar.js
@@ -48,11 +48,11 @@ selfoss.events.entriesToolbar = function(parent) {
     if (shares.length > 0) {
         if (parent.find('.entry-toolbar').has('button.entry-share' + shares[0]).length == 0) {
             // add the share toolbar entries
-            parent.find('.entry-smartphone-share button.entry-newwindow').after(selfoss.shares.buildLinks(shares, function(name) {
-                return '<button class="entry-share entry-share' + name + '" title="' + name + '"><img class="entry-share" title="' + name + '" src="images/' + name + '.png" height="16" width="16">' + name + '</button>';
+            parent.find('.entry-smartphone-share button.entry-newwindow').parent().after(selfoss.shares.buildLinks(shares, function(name) {
+                return '<li><button type="button" class="entry-share entry-share' + name + '" title="' + name + '" aria-label="' + name + '"><img class="entry-share" title="' + name + '" alt="' + name + '" src="images/' + name + '.png" height="16" width="16">' + name + '</button></li>';
             }));
-            parent.find('.entry-toolbar button.entry-next').after(selfoss.shares.buildLinks(shares, function(name) {
-                return '<button class="entry-share entry-share' + name + '"><img title="' + name + '" src="images/' + name + '.png" height="16" width="16"></button>';
+            parent.find('.entry-toolbar button.entry-next').parent().after(selfoss.shares.buildLinks(shares, function(name) {
+                return '<li><button type="button" class="entry-share entry-share' + name + '" aria-label="' + name + '"><img title="' + name + '" alt="' + name + '" src="images/' + name + '.png" height="16" width="16"></button></li>';
             }));
             // hookup the share icon click events
             for (var i = 0; i < shares.length; i++) {
