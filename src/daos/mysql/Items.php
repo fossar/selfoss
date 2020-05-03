fossar/selfoss

commit 53569e1c613ef56c70f3206c05e6713c0d8fc6a7

daos: Inject some dependencies
We create an interface for each DAO type and configure the DI container
to return the implementation corresponding to the backend selected
in the configuration.
Top-level DAOs will then just ask for the interface and Dice will
provide the correct instance.

src/daos/mysql/Items.php
@@ -12,12 +12,19 @@ use DateTime;
  * @author     Tobias Zeising <tobias.zeising@aditu.de>
  * @author     Harald Lapp <harald.lapp@gmail.com>
  */
-class Items {
+class Items implements \daos\ItemsInterface {
     /** @var bool indicates whether last run has more results or not */
     protected $hasMore = false;
 
     /** @var class-string SQL helper */
     protected static $stmt = Statements::class;
+
+    /** @var \daos\Database database connection */
+    protected $database;
+
+    public function __construct(\daos\Database $database) {
+        $this->database = $database;
+    }
 
     /**
      * mark item as read
@@ -36,7 +43,7 @@ class Items {
         }
 
         // i used string concatenation after validating $id
-        \F3::get('db')->exec('UPDATE ' . \F3::get('db_prefix') . "items SET unread=? WHERE id IN ($id)", false);
+        $this->database->exec('UPDATE ' . \F3::get('db_prefix') . "items SET unread=? WHERE id IN ($id)", false);
     }
 
     /**
@@ -52,7 +59,7 @@ class Items {
         } elseif (!is_numeric($id)) {
             return;
         }
-        \F3::get('db')->exec('UPDATE ' . \F3::get('db_prefix') . "items SET unread=? WHERE id IN ($id)", true);
+        $this->database->exec('UPDATE ' . \F3::get('db_prefix') . "items SET unread=? WHERE id IN ($id)", true);
     }
 
     /**
@@ -63,7 +70,7 @@ class Items {
      * @return void
      */
     public function starr($id) {
-        \F3::get('db')->exec('UPDATE ' . \F3::get('db_prefix') . 'items SET starred=:bool WHERE id=:id', [
+        $this->database->exec('UPDATE ' . \F3::get('db_prefix') . 'items SET starred=:bool WHERE id=:id', [
             ':bool' => true,
             ':id' => $id
         ]);
@@ -77,7 +84,7 @@ class Items {
      * @return void
      */
     public function unstarr($id) {
-        \F3::get('db')->exec('UPDATE ' . \F3::get('db_prefix') . 'items SET starred=:bool WHERE id=:id', [
+        $this->database->exec('UPDATE ' . \F3::get('db_prefix') . 'items SET starred=:bool WHERE id=:id', [
             ':bool' => false,
             ':id' => $id
         ]);
@@ -91,7 +98,7 @@ class Items {
      * @return void
      */
     public function add($values) {
-        \F3::get('db')->exec('INSERT INTO ' . \F3::get('db_prefix') . 'items (
+        $this->database->exec('INSERT INTO ' . \F3::get('db_prefix') . 'items (
                     datetime,
                     title,
                     content,
@@ -140,7 +147,7 @@ class Items {
      * @return bool
      */
     public function exists($uid) {
-        $res = \F3::get('db')->exec('SELECT COUNT(*) AS amount FROM ' . \F3::get('db_prefix') . 'items WHERE uid=:uid',
+        $res = $this->database->exec('SELECT COUNT(*) AS amount FROM ' . \F3::get('db_prefix') . 'items WHERE uid=:uid',
             [':uid' => [$uid, \PDO::PARAM_STR]]);
 
         return $res[0]['amount'] > 0;
@@ -161,10 +168,10 @@ class Items {
         }
 
         array_walk($itemsInFeed, function(&$value) {
-            $value = \F3::get('db')->quote($value);
+            $value = $this->database->quote($value);
         });
-        $query = 'SELECT id, uid AS uid FROM ' . \F3::get('db_prefix') . 'items WHERE source = ' . \F3::get('db')->quote($sourceId) . ' AND uid IN (' . implode(',', $itemsInFeed) . ')';
-        $res = \F3::get('db')->query($query);
+        $query = 'SELECT id, uid AS uid FROM ' . \F3::get('db_prefix') . 'items WHERE source = ' . $this->database->quote($sourceId) . ' AND uid IN (' . implode(',', $itemsInFeed) . ')';
+        $res = $this->database->query($query);
         if ($res) {
             $all = $res->fetchAll();
             foreach ($all as $row) {
@@ -185,7 +192,7 @@ class Items {
      */
     public function updateLastSeen(array $itemIds) {
         $stmt = static::$stmt;
-        \F3::get('db')->exec('UPDATE ' . \F3::get('db_prefix') . 'items SET lastseen = CURRENT_TIMESTAMP
+        $this->database->exec('UPDATE ' . \F3::get('db_prefix') . 'items SET lastseen = CURRENT_TIMESTAMP
             WHERE ' . $stmt::intRowMatches('id', $itemIds));
     }
 
@@ -198,11 +205,11 @@ class Items {
      */
     public function cleanup(DateTime $date = null) {
         $stmt = static::$stmt;
-        \F3::get('db')->exec('DELETE FROM ' . \F3::get('db_prefix') . 'items
+        $this->database->exec('DELETE FROM ' . \F3::get('db_prefix') . 'items
             WHERE source NOT IN (
                 SELECT id FROM ' . \F3::get('db_prefix') . 'sources)');
         if ($date !== null) {
-            \F3::get('db')->exec('DELETE FROM ' . \F3::get('db_prefix') . 'items
+            $this->database->exec('DELETE FROM ' . \F3::get('db_prefix') . 'items
                 WHERE ' . $stmt::isFalse('starred') . ' AND lastseen<:date',
                     [':date' => $date->format('Y-m-d') . ' 00:00:00']
             );
@@ -320,7 +327,7 @@ class Items {
         }
 
         // first check whether more items are available
-        $result = \F3::get('db')->exec('SELECT items.id
+        $result = $this->database->exec('SELECT items.id
                    FROM ' . \F3::get('db_prefix') . 'items AS items, ' . \F3::get('db_prefix') . 'sources AS sources
                    WHERE items.source=sources.id AND ' . $where_sql . '
                    LIMIT 1 OFFSET ' . ($options['offset'] + $options['items']), $params);
@@ -352,7 +359,7 @@ class Items {
             $query = "$select $where_sql $order_sql LIMIT " . $options['items'] . ' OFFSET ' . $options['offset'];
         }
 
-        return $stmt::ensureRowTypes(\F3::get('db')->exec($query, $params), [
+        return $stmt::ensureRowTypes($this->database->exec($query, $params), [
             'id' => \daos\PARAM_INT,
             'unread' => \daos\PARAM_BOOL,
             'starred' => \daos\PARAM_BOOL,
@@ -402,7 +409,7 @@ class Items {
             'since' => [$since->format(\DateTime::ATOM), \PDO::PARAM_STR]
         ];
 
-        return $stmt::ensureRowTypes(\F3::get('db')->exec($query, $params), [
+        return $stmt::ensureRowTypes($this->database->exec($query, $params), [
             'id' => \daos\PARAM_INT,
             'unread' => \daos\PARAM_BOOL,
             'starred' => \daos\PARAM_BOOL,
@@ -418,7 +425,7 @@ class Items {
     public function lowestIdOfInterest() {
         $stmt = static::$stmt;
         $lowest = $stmt::ensureRowTypes(
-            \F3::get('db')->exec(
+            $this->database->exec(
                 'SELECT id FROM ' . \F3::get('db_prefix') . 'items AS items
                  WHERE ' . $stmt::isTrue('unread') .
                     ' OR ' . $stmt::isTrue('starred') .
@@ -440,7 +447,7 @@ class Items {
     public function lastId() {
         $stmt = static::$stmt;
         $lastId = $stmt::ensureRowTypes(
-            \F3::get('db')->exec(
+            $this->database->exec(
                 'SELECT id FROM ' . \F3::get('db_prefix') . 'items AS items
                  ORDER BY id DESC LIMIT 1'),
             ['id' => \daos\PARAM_INT]
@@ -459,7 +466,7 @@ class Items {
      */
     public function getThumbnails() {
         $thumbnails = [];
-        $result = \F3::get('db')->exec('SELECT thumbnail
+        $result = $this->database->exec('SELECT thumbnail
                    FROM ' . \F3::get('db_prefix') . 'items
                    WHERE thumbnail!=""');
         foreach ($result as $thumb) {
@@ -476,7 +483,7 @@ class Items {
      */
     public function getIcons() {
         $icons = [];
-        $result = \F3::get('db')->exec('SELECT icon
+        $result = $this->database->exec('SELECT icon
                    FROM ' . \F3::get('db_prefix') . 'items
                    WHERE icon!=""');
         foreach ($result as $icon) {
@@ -494,7 +501,7 @@ class Items {
      * @return bool true if thumbnail is still in use
      */
     public function hasThumbnail($thumbnail) {
-        $res = \F3::get('db')->exec('SELECT count(*) AS amount
+        $res = $this->database->exec('SELECT count(*) AS amount
                    FROM ' . \F3::get('db_prefix') . 'items
                    WHERE thumbnail=:thumbnail',
                   [':thumbnail' => $thumbnail]);
@@ -514,7 +521,7 @@ class Items {
      * @return bool true if icon is still in use
      */
     public function hasIcon($icon) {
-        $res = \F3::get('db')->exec('SELECT count(*) AS amount
+        $res = $this->database->exec('SELECT count(*) AS amount
                    FROM ' . \F3::get('db_prefix') . 'items
                    WHERE icon=:icon',
                   [':icon' => $icon]);
@@ -564,7 +571,7 @@ class Items {
             return null;
         }
 
-        $res = \F3::get('db')->exec('SELECT icon FROM ' . \F3::get('db_prefix') . 'items WHERE source=:sourceid AND icon!=\'\' AND icon IS NOT NULL ORDER BY ID DESC LIMIT 1',
+        $res = $this->database->exec('SELECT icon FROM ' . \F3::get('db_prefix') . 'items WHERE source=:sourceid AND icon!=\'\' AND icon IS NOT NULL ORDER BY ID DESC LIMIT 1',
             [':sourceid' => $sourceid]);
         if (count($res) === 1) {
             return $res[0]['icon'];
@@ -580,7 +587,7 @@ class Items {
      */
     public function numberOfUnread() {
         $stmt = static::$stmt;
-        $res = \F3::get('db')->exec('SELECT count(*) AS amount
+        $res = $this->database->exec('SELECT count(*) AS amount
                    FROM ' . \F3::get('db_prefix') . 'items
                    WHERE ' . $stmt::isTrue('unread'));
 
@@ -594,7 +601,7 @@ class Items {
      */
     public function stats() {
         $stmt = static::$stmt;
-        $res = \F3::get('db')->exec('SELECT
+        $res = $this->database->exec('SELECT
             COUNT(*) AS total,
             ' . $stmt::sumBool('unread') . ' AS unread,
             ' . $stmt::sumBool('starred') . ' AS starred
@@ -614,7 +621,7 @@ class Items {
      * @return string timestamp
      */
     public function lastUpdate() {
-        $res = \F3::get('db')->exec('SELECT
+        $res = $this->database->exec('SELECT
             MAX(updatetime) AS last_update_time
             FROM ' . \F3::get('db_prefix') . 'items;');
 
@@ -630,7 +637,7 @@ class Items {
      */
     public function statuses(DateTime $since) {
         $stmt = static::$stmt;
-        $res = \F3::get('db')->exec('SELECT id, unread, starred
+        $res = $this->database->exec('SELECT id, unread, starred
             FROM ' . \F3::get('db_prefix') . 'items
             WHERE ' . \F3::get('db_prefix') . 'items.updatetime > :since;',
                 [':since' => [$since->format(DateTime::ATOM), \PDO::PARAM_STR]]);
@@ -709,13 +716,13 @@ class Items {
         }
 
         if ($sql) {
-            \F3::get('db')->begin();
+            $this->database->begin();
             foreach ($sql as $id => $q) {
                 $params = [
                     ':id' => [$id, \PDO::PARAM_INT],
                     ':statusUpdate' => [$q['datetime'], \PDO::PARAM_STR]
                 ];
-                $updated = \F3::get('db')->exec(
+                $updated = $this->database->exec(
                     'UPDATE ' . \F3::get('db_prefix') . 'items
                     SET ' . implode(', ', array_values($q['updates'])) . '
                     WHERE id = :id AND updatetime < :statusUpdate', $params);
@@ -723,13 +730,13 @@ class Items {
                     // entry status was updated in between so updatetime must
                     // be updated to ensure client side consistency of
                     // statuses.
-                    \F3::get('db')->exec(
+                    $this->database->exec(
                         'UPDATE ' . \F3::get('db_prefix') . 'items
                          SET ' . $stmt::rowTouch('updatetime') . '
                          WHERE id = :id', [':id' => [$id, \PDO::PARAM_INT]]);
                 }
             }
-            \F3::get('db')->commit();
+            $this->database->commit();
         }
     }
 }

src/daos/Items.php
@@ -2,6 +2,8 @@
 
 namespace daos;
 
+use helpers\Authentication;
+
 /**
  * Class for accessing persistent saved items
  *
@@ -10,19 +12,21 @@ namespace daos;
  * @author     Harald Lapp <harald.lapp@gmail.com>
  * @author     Tobias Zeising <tobias.zeising@aditu.de>
  */
-class Items extends Database {
-    /** @var object Instance of backend specific items class */
-    private $backend = null;
+class Items {
+    /** @var ItemsInterface Instance of backend specific items class */
+    private $backend;
+
+    /** @var Authentication authentication helper */
+    private $authentication;
 
     /**
      * Constructor.
      *
      * @return void
      */
-    public function __construct() {
-        $class = 'daos\\' . \F3::get('db_type') . '\\Items';
-        $this->backend = new $class();
-        parent::__construct();
+    public function __construct(Authentication $authentication, ItemsInterface $backend) {
+        $this->authentication = $authentication;
+        $this->backend = $backend;
     }
 
     /**
@@ -78,7 +82,7 @@ class Items extends Database {
         $items = $this->backend->get($options);
 
         // remove private posts with private tags
-        if (!\F3::get('auth')->showPrivateTags()) {
+        if (!$this->authentication->showPrivateTags()) {
             foreach ($items as $idx => $item) {
                 foreach ($item['tags'] as $tag) {
                     if (strpos(trim($tag), '@') === 0) {

index.php
@@ -8,10 +8,6 @@ if ($lang != '0' && $lang != '') {
     $f3->set('LANGUAGE', $lang);
 }
 
-// init authentication
-// TODO: remove once we let DI container create database
-$f3->set('auth', $f3->get('CONTAINER')(helpers\Authentication::class));
-
 // define routes
 
 // all users

src/common.php
@@ -65,9 +65,89 @@ foreach ($interpolatedKeys as $key) {
 
 $dice = new Dice();
 
-$dice->addRule(helpers\Authentication::class, [
+// DI rules
+// Choose database implementation based on config
+$substitutions = [
+    'substitutions' => [
+        daos\DatabaseInterface::class => ['instance' => 'daos\\' . $f3->get('db_type') . '\\Database'],
+        daos\ItemsInterface::class => ['instance' => 'daos\\' . $f3->get('db_type') . '\\Items'],
+        daos\SourcesInterface::class => ['instance' => 'daos\\' . $f3->get('db_type') . '\\Sources'],
+        daos\TagsInterface::class => ['instance' => 'daos\\' . $f3->get('db_type') . '\\Tags'],
+    ]
+];
+
+$shared = array_merge($substitutions, [
     'shared' => true,
 ]);
+
+$dice->addRule(helpers\Authentication::class, $shared);
+
+// Database bridges
+$dice->addRule(daos\Database::class, $shared);
+$dice->addRule(daos\Items::class, $shared);
+$dice->addRule(daos\Sources::class, $shared);
+$dice->addRule(daos\Tags::class, $shared);
+
+// Database implementation
+$dice->addRule(daos\DatabaseInterface::class, $shared);
+$dice->addRule(daos\ItemsInterface::class, $shared);
+$dice->addRule(daos\SourcesInterface::class, $shared);
+$dice->addRule(daos\TagsInterface::class, $shared);
+
+// Database connection
+if ($f3->get('db_type') === 'sqlite') {
+    $db_file = $f3->get('db_file');
+
+    // create empty database file if it does not exist
+    if (!is_file($db_file)) {
+        touch($db_file);
+    }
+
+    $dsn = 'sqlite:' . $db_file;
+    $dbParams = [
+        $dsn
+    ];
+} elseif ($f3->get('db_type') === 'mysql') {
+    $host = $f3->get('db_host');
+    $port = $f3->get('db_port');
+    $database = $f3->get('db_database');
+
+    if ($port) {
+        $dsn = "mysql:host=$host; port=$port; dbname=$database";
+    } else {
+        $dsn = "mysql:host=$host; dbname=$database";
+    }
+
+    $dbParams = [
+        $dsn,
+        $f3->get('db_username'),
+        $f3->get('db_password'),
+        [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4;']
+    ];
+} elseif ($f3->get('db_type') === 'pgsql') {
+    $host = $f3->get('db_host');
+    $port = $f3->get('db_port');
+    $database = $f3->get('db_database');
+
+    if ($port) {
+        $dsn = "pgsql:host=$host; port=$port; dbname=$database";
+    } else {
+        $dsn = "pgsql:host=$host; dbname=$database";
+    }
+
+    $dbParams = [
+        $dsn,
+        $f3->get('db_username'),
+        $f3->get('db_password')
+    ];
+}
+
+$dice->addRule(DB\SQL::class, array_merge($shared, [
+    'constructParams' => $dbParams
+]));
+
+// Fallback rule
+$dice->addRule('*', $substitutions);
 
 $f3->set('CONTAINER', function($class) use ($dice) {
     return $dice->create($class);

src/daos/Database.php
@@ -15,26 +15,30 @@ const PARAM_CSV = 3;
  * @author     Tobias Zeising <tobias.zeising@aditu.de>
  */
 class Database {
-    /** @var object Instance of backend specific database access class */
+    /** @var DatabaseInterface Instance of backend specific database access class */
     private $backend = null;
 
     /**
      * establish connection and
      * create undefined tables
-     *
-     * @return void
      */
-    public function __construct() {
-        $class = 'daos\\' . \F3::get('db_type') . '\\Database';
-        $this->backend = new $class();
+    public function __construct(DatabaseInterface $backend) {
+        $this->backend = $backend;
     }
 
     /**
-     * optimize database by database own optimize statement
+     * pass any method call to the backend.
      *
-     * @return  void
+     * @param string $name name of the function
+     * @param array $args arguments
+     *
+     * @return mixed methods return value
      */
-    public function optimize() {
-        $this->backend->optimize();
+    public function __call($name, $args) {
+        if (method_exists($this->backend, $name)) {
+            return call_user_func_array([$this->backend, $name], $args);
+        } else {
+            \F3::get('logger')->error('Unimplemented method for ' . \F3::get('db_type') . ': ' . $name);
+        }
     }
 }

src/daos/DatabaseInterface.php
@@ -1,0 +1,67 @@
+<?php
+
+namespace daos;
+
+/**
+ * Interface describing database backend.
+ */
+interface DatabaseInterface {
+    /**
+     * Execute SQL statement(s)
+     *
+     * @param string|string[] $cmds
+     * @param string|array $args
+     *
+     * @return array|int|false
+     */
+    public function exec($cmds, $args = null);
+
+    /**
+     * wrap insert statement to return id
+     *
+     * @param string $query sql statement
+     * @param array $params sql params
+     *
+     * @return int id after insert
+     */
+    public function insert($query, array $params);
+
+    /**
+     * Quote string
+     *
+     * @param mixed $val
+     * @param int $type
+     * @param mixed $value
+     *
+     * @return string
+     */
+    public function quote($value, $type = \PDO::PARAM_STR);
+
+    /**
+     * Begin SQL transaction
+     *
+     * @return bool
+     */
+    public function begin();
+
+    /**
+     * Rollback SQL transaction
+     *
+     * @return bool
+     */
+    public function rollback();
+
+    /**
+     * Commit SQL transaction
+     *
+     * @return bool
+     */
+    public function commit();
+
+    /**
+     * Optimize database using its own optimize statement.
+     *
+     * @return void
+     */
+    public function optimize();
+}

src/daos/ItemsInterface.php
@@ -1,0 +1,226 @@
+<?php
+
+namespace daos;
+
+use DateTime;
+
+/**
+ * Interface describing concrete DAO for working with items.
+ */
+interface ItemsInterface {
+    /**
+     * mark item as read
+     *
+     * @param int $id
+     *
+     * @return void
+     */
+    public function mark($id);
+
+    /**
+     * mark item as unread
+     *
+     * @param int $id
+     *
+     * @return void
+     */
+    public function unmark($id);
+
+    /**
+     * starr item
+     *
+     * @param int $id the item
+     *
+     * @return void
+     */
+    public function starr($id);
+
+    /**
+     * unstarr item
+     *
+     * @param int $id the item
+     *
+     * @return void
+     */
+    public function unstarr($id);
+
+    /**
+     * add new item
+     *
+     * @param array $values
+     *
+     * @return void
+     */
+    public function add($values);
+
+    /**
+     * checks whether an item with given
+     * uid exists or not
+     *
+     * @param string $uid
+     *
+     * @return bool
+     */
+    public function exists($uid);
+
+    /**
+     * search whether given uids are already in database or not
+     *
+     * @param array $itemsInFeed list with ids for checking whether they are already in database or not
+     * @param int $sourceId the id of the source to search for the items
+     *
+     * @return array with all existing uids from itemsInFeed (array (uid => id););
+     */
+    public function findAll($itemsInFeed, $sourceId);
+
+    /**
+     * Update the time items were last seen in the feed to prevent unwanted cleanup.
+     *
+     * @param int[] $itemIds ids of items to update
+     *
+     * @return void
+     */
+    public function updateLastSeen(array $itemIds);
+
+    /**
+     * cleanup orphaned and old items
+     *
+     * @param ?DateTime $date date to delete all items older than this value
+     *
+     * @return void
+     */
+    public function cleanup(DateTime $date = null);
+
+    /**
+     * returns items
+     *
+     * @param mixed $options search, offset and filter params
+     *
+     * @return mixed items as array
+     */
+    public function get($options = []);
+
+    /**
+     * returns whether more items for last given
+     * get call are available
+     *
+     * @return bool
+     */
+    public function hasMore();
+
+    /**
+     * Obtain new or changed items in the database for synchronization with clients.
+     *
+     * @param int $sinceId id of last seen item
+     * @param DateTime $notBefore cut off time stamp
+     * @param DateTime $since timestamp of last seen item
+     * @param int $howMany
+     *
+     * @return array of items
+     */
+    public function sync($sinceId, DateTime $notBefore, DateTime $since, $howMany);
+
+    /**
+     * Lowest id of interest
+     *
+     * @return int lowest id of interest
+     */
+    public function lowestIdOfInterest();
+
+    /**
+     * Last id in db
+     *
+     * @return int last id in db
+     */
+    public function lastId();
+
+    /**
+     * return all thumbnails
+     *
+     * @return string[] array with thumbnails
+     */
+    public function getThumbnails();
+
+    /**
+     * return all icons
+     *
+     * @return string[] array with all icons
+     */
+    public function getIcons();
+
+    /**
+     * return all thumbnails
+     *
+     * @param string $thumbnail name
+     *
+     * @return bool true if thumbnail is still in use
+     */
+    public function hasThumbnail($thumbnail);
+
+    /**
+     * return all icons
+     *
+     * @param string $icon file
+     *
+     * @return bool true if icon is still in use
+     */
+    public function hasIcon($icon);
+
+    /**
+     * test if the value of a specified field is valid
+     *
+     * @param   string      $name
+     * @param   mixed       $value
+     *
+     * @return  bool
+     */
+    public function isValid($name, $value);
+
+    /**
+     * returns the icon of the last fetched item.
+     *
+     * @param int $sourceid id of the source
+     *
+     * @return ?string
+     */
+    public function getLastIcon($sourceid);
+
+    /**
+     * returns the amount of entries in database which are unread
+     *
+     * @return int amount of entries in database which are unread
+     */
+    public function numberOfUnread();
+
+    /**
+     * returns the amount of total, unread, starred entries in database
+     *
+     * @return array mount of total, unread, starred entries in database
+     */
+    public function stats();
+
+    /**
+     * returns the datetime of the last item update or user action in db
+     *
+     * @return string timestamp
+     */
+    public function lastUpdate();
+
+    /**
+     * returns the statuses of items last update
+     *
+     * @param DateTime $since minimal date of returned items
+     *
+     * @return array of unread, starred, etc. status of specified items
+     */
+    public function statuses(DateTime $since);
+
+    /**
+     * bulk update of item status
+     *
+     * @param array $statuses array of statuses updates
+     *
+     * @return void
+     */
+    public function bulkStatusUpdate(array $statuses);
+}

src/daos/Sources.php
@@ -2,6 +2,9 @@
 
 namespace daos;
 
+use helpers\Authentication;
+use helpers\SpoutLoader;
+
 /**
  * Class for accessing persistent saved sources
  *
@@ -11,19 +14,25 @@ namespace daos;
  * @author     Daniel Seither <post@tiwoc.de>
  * @author     Tobias Zeising <tobias.zeising@aditu.de>
  */
-class Sources extends Database {
-    /** @var object Instance of backend specific sources class */
-    private $backend = null;
+class Sources {
+    /** @var SourcesInterface Instance of backend specific sources class */
+    private $backend;
+
+    /** @var Authentication authentication helper */
+    private $authentication;
+
+    /** @var SpoutLoader spout loader */
+    private $spoutLoader;
 
     /**
      * Constructor.
      *
      * @return void
      */
-    public function __construct() {
-        $class = 'daos\\' . \F3::get('db_type') . '\\Sources';
-        $this->backend = new $class();
-        parent::__construct();
+    public function __construct(Authentication $authentication, SourcesInterface $backend, SpoutLoader $spoutLoader) {
+        $this->authentication = $authentication;
+        $this->backend = $backend;
+        $this->spoutLoader = $spoutLoader;
     }
 
     /**
@@ -46,7 +55,7 @@ class Sources extends Database {
         $sources = $this->backend->get($id);
         if ($id === null) {
             // remove items with private tags
-            if (!\F3::get('auth')->showPrivateTags()) {
+            if (!$this->authentication->showPrivateTags()) {
                 foreach ($sources as $idx => $source) {
                     foreach ($source['tags'] as $tag) {
                         if (strpos(trim($tag), '@') === 0) {
@@ -82,8 +91,7 @@ class Sources extends Database {
         }
 
         // spout type
-        $spoutLoader = new \helpers\SpoutLoader();
-        $spout = $spoutLoader->get($spout);
+        $spout = $this->spoutLoader->get($spout);
         if ($spout === null) {
             $result['spout'] = 'invalid spout type';
         } else { // check params

commit f3993503668cbf315534d89d66d77898410a4bb4

daos: Fix check if item is already downloaded
I accidently broke this in 53569e1c613ef56c70f3206c05e6713c0d8fc6a7.
It was failing because database no longer has query method.

Fixes: https://github.com/SSilence/selfoss/issues/1186

src/daos/mysql/Items.php
@@ -176,13 +176,10 @@ class Items implements \daos\ItemsInterface {
             $value = $this->database->quote($value);
         });
         $query = 'SELECT id, uid AS uid FROM ' . \F3::get('db_prefix') . 'items WHERE source = ' . $this->database->quote($sourceId) . ' AND uid IN (' . implode(',', $itemsInFeed) . ')';
-        $res = $this->database->query($query);
-        if ($res) {
-            $all = $res->fetchAll();
-            foreach ($all as $row) {
-                $uid = $row['uid'];
-                $itemsFound[$uid] = $row['id'];
-            }
+        $res = $this->database->exec($query);
+        foreach ($res as $row) {
+            $uid = $row['uid'];
+            $itemsFound[$uid] = $row['id'];
         }
 
         return $itemsFound;