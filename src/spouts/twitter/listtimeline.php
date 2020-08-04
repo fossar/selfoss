cc9bdf121ca510e1f5621c0266e539748c51e595
Jan Tojnar <jtojnar@gmail.com>
2020-08-04 03:25:49 +0200

    spouts\twitter: Fix timeline listing
    During the DI rewrite, we forgot to call parent constructor.

--- src/spouts/twitter/listtimeline.php
+++ src/spouts/twitter/listtimeline.php
@@ -10,54 +10,52 @@ namespace spouts\twitter;
  * @author     Nicola Malizia <unnikked@gmail.com>
  */
 class listtimeline extends \spouts\twitter\usertimeline {
-    public function __construct() {
-        $this->name = 'Twitter: list timeline';
-        $this->description = 'Fetch the timeline of a given list.';
-        $this->params = [
-            'consumer_key' => [
-                'title' => 'Consumer Key',
-                'type' => 'text',
-                'default' => '',
-                'required' => true,
-                'validation' => ['notempty']
-            ],
-            'consumer_secret' => [
-                'title' => 'Consumer Secret',
-                'type' => 'password',
-                'default' => '',
-                'required' => true,
-                'validation' => ['notempty']
-            ],
-            'access_token' => [
-                'title' => 'Access Token (optional)',
-                'type' => 'text',
-                'default' => '',
-                'required' => false,
-                'validation' => []
-            ],
-            'access_token_secret' => [
-                'title' => 'Access Token Secret (optional)',
-                'type' => 'password',
-                'default' => '',
-                'required' => false,
-                'validation' => []
-            ],
-            'slug' => [
-                'title' => 'List Slug',
-                'type' => 'text',
-                'default' => '',
-                'required' => true,
-                'validation' => ['notempty']
-            ],
-            'owner_screen_name' => [
-                'title' => 'Username',
-                'type' => 'text',
-                'default' => '',
-                'required' => true,
-                'validation' => ['notempty']
-            ]
-        ];
-    }
+    public $name = 'Twitter: list timeline';
+    public $description = 'Fetch the timeline of a given list.';
+    public $params = [
+        'consumer_key' => [
+            'title' => 'Consumer Key',
+            'type' => 'text',
+            'default' => '',
+            'required' => true,
+            'validation' => ['notempty']
+        ],
+        'consumer_secret' => [
+            'title' => 'Consumer Secret',
+            'type' => 'password',
+            'default' => '',
+            'required' => true,
+            'validation' => ['notempty']
+        ],
+        'access_token' => [
+            'title' => 'Access Token (optional)',
+            'type' => 'text',
+            'default' => '',
+            'required' => false,
+            'validation' => []
+        ],
+        'access_token_secret' => [
+            'title' => 'Access Token Secret (optional)',
+            'type' => 'password',
+            'default' => '',
+            'required' => false,
+            'validation' => []
+        ],
+        'slug' => [
+            'title' => 'List Slug',
+            'type' => 'text',
+            'default' => '',
+            'required' => true,
+            'validation' => ['notempty']
+        ],
+        'owner_screen_name' => [
+            'title' => 'Username',
+            'type' => 'text',
+            'default' => '',
+            'required' => true,
+            'validation' => ['notempty']
+        ]
+    ];
 
     public function load(array $params) {
         $this->client = $this->getHttpClient($params['consumer_key'], $params['consumer_secret'], $params['access_token'], $params['access_token_secret']);
