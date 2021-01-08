1c26ca4e0e2e16b8ac4a83c2efce15a8d8bb26cf
Jan Tojnar <jtojnar@gmail.com>
2021-01-08 17:36:25 +0100

    Prevent double-escaped entities in the navbar
    For legacy reasons, we escape tags and source titles on the server.
    
    Previously, we used server side templates where we did not escape the values but with a switch to client-side templates in https://github.com/fossar/selfoss/pull/1184, we started escaping those texts in the templates. This resulted in double escaped ampersands being shown to users.
    
    Let’s unescape those values client-side for now since unescaping them in the database or on the server would break backwards compatibility for third party clients.
    
    Fixes: https://github.com/fossar/selfoss/issues/1241

--- assets/templates/nav-sources.js
+++ assets/templates/nav-sources.js
@@ -1,7 +1,9 @@
+import { unescape } from 'html-escaper';
+
 export default ({sources}) => sources.map(source =>
     <li>
         <a href="#" class={(source.unread > 0) ? 'unread' : ''} data-source-id={`${source.id}`}>
-            <span class="nav-source">{`${source.title}`}</span>
+            <span class="nav-source">{`${unescape(source.title)}`}</span>
             <span class="unread">{`${(source.unread > 0) ? `${source.unread}` : ''}`}</span>
         </a>
     </li>
--- assets/templates/nav-tags.js
+++ assets/templates/nav-tags.js
@@ -1,10 +1,12 @@
+import { unescape } from 'html-escaper';
+
 export default ({tags}) =>
     [
         <li><a class="active nav-tags-all" href="#">{selfoss.ui._('alltags')}</a></li>
     ].concat(tags.map(tag =>
         <li>
             <a href="#">
-                <span class="tag">{`${tag.tag}`}</span>
+                <span class="tag">{`${unescape(tag.tag)}`}</span>
                 <span class="unread">{`${(tag.unread > 0) ? tag.unread : ''}`}</span>
                 <span class="color" style={`background-color: ${tag.color}`}></span>
             </a>
