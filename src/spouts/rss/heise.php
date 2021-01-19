cdaa7c9e418595be048101c409de0245879eaee4
Jan Tojnar <jtojnar@gmail.com>
2021-01-19 14:33:11 +0100

    spouts: Port Heise to Graby
    The old spout does not seem to extract content anymore but Graby does.

--- src/spouts/rss/heise.php
+++ src/spouts/rss/heise.php
@@ -10,7 +10,7 @@ namespace spouts\rss;
  * @author     Tobias Zeising <tobias.zeising@aditu.de>
  * @author     Daniel Seither <post@tiwoc.de>
  */
-class heise extends feed {
+class heise extends fulltextrss {
     /** @var string name of spout */
     public $name = '[German] heise.de';
 
@@ -66,88 +66,11 @@ class heise extends feed {
         'hh' => 'https://www.heise.de/hardware-hacks/rss/hardware-hacks-atom.xml',
     ];
 
-    /**
-     * delimiters of the article text
-     *
-     * elements: start tag, attribute of start tag, value of start tag attribute, end
-     */
-    private $textDivs = [
-        ['div', 'class', 'meldung_wrapper', '<!-- AUTHOR-DATA-MARKER-BEGIN'], // main, ix, mac, mobil, sec, net, open, dev, resale, foto, hh articles
-        ['p', 'class', 'artikel_datum', '<p class="artikel_option">'],        // ct
-        ['div', 'class', 'aufmacher', '<!-- AUTHOR-DATA-MARKER-BEGIN'],       // tr
-        ['div', 'class', 'datum_autor', '<div class="artikel_fuss">'],        // mac
-        ['p', 'class', 'vorlauftext', '<div class="artikel_fuss">'],          // mobil
-        ['div', 'id', 'blocon', '</div>'],                                    // tp
-        ['div', 'class', 'mar0', '<div id="breadcrumb">'],                    // some tp articles
-        ['span', 'class', 'date', '<div xmlns:v="http://rdf'],                // tp
-        ['div', 'class', 'artikel_content', '<div class="artikel_fuss">'],    // resale
-        ['div', 'id', 'artikel_shortnews', '<p class="editor">'],             // autos
-        ['div', 'id', 'projekte', '<div id="artikelfuss">'],                  // hh projects
-        ['div', 'id', 'artikel', '<div id="artikelfuss">'],                   // some hh articles
-    ];
-
-    /**
-     * htmLawed configuration
-     */
-    private $htmLawedConfig = [
-        'abs_url' => 1,
-        'base_url' => 'https://www.heise.de/',
-        'comment' => 1,
-        'safe' => 1,
-    ];
-
     public function load(array $params) {
         parent::load(['url' => $this->getXmlUrl($params)]);
     }
 
     public function getXmlUrl(array $params) {
         return $this->feedUrls[$params['section']];
-    }
-
-    public function getContent() {
-        if ($this->items !== null && $this->valid()) {
-            $originalContent = file_get_contents($this->getLink());
-            foreach ($this->textDivs as $div) {
-                $content = $this->getTag($div[1], $div[2], $originalContent, $div[0], $div[3]);
-                if (is_array($content) && count($content) >= 1) {
-                    return htmLawed($content[0], $this->htmLawedConfig);
-                }
-            }
-        }
-
-        return parent::getContent();
-    }
-
-    /**
-     * get tag by attribute
-     * taken from http://www.catswhocode.com/blog/15-php-regular-expressions-for-web-developers
-     *
-     * @param string $attr attribute
-     * @param string $value necessary value
-     * @param string $xml data string
-     * @param ?string $tag optional tag
-     * @param ?string $end optional ending
-     *
-     * @return string[] content
-     */
-    private function getTag($attr, $value, $xml, $tag = null, $end = null) {
-        if ($tag === null) {
-            $tag = '\w+';
-        } else {
-            $tag = preg_quote($tag);
-        }
-
-        if ($end === null) {
-            $end = '</\1>';
-        } else {
-            $end = preg_quote($end);
-        }
-
-        $attr = preg_quote($attr);
-        $value = preg_quote($value);
-        $tag_regex = '|<(' . $tag . ')[^>]*' . $attr . '\s*=\s*([\'"])' . $value . '\2[^>]*>(.*?)' . $end . '|ims';
-        preg_match_all($tag_regex, $xml, $matches, PREG_PATTERN_ORDER);
-
-        return $matches[3];
     }
 }
