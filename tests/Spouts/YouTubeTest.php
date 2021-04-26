e20af8380e01e28028a97b38c99f0a2bd7adee1e
Jan Tojnar <jtojnar@gmail.com>
2021-04-26 08:35:31 +0200

    tests: Add test for YouTube spout

---
+++ tests/Spouts/YouTubeTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Tests\Spouts;
+
+use Dice\Dice;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use helpers\Configuration;
+use helpers\WebClient;
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+use spouts\youtube\youtube;
+
+final class YouTubeTest extends TestCase {
+    /**
+     * @dataProvider dataProvider
+     *
+     * @param string $url
+     * @param string $feedTitle
+     * @param string $firstItemTitle
+     */
+    public function testBasic($url, $feedTitle, $firstItemTitle) {
+        $cachedFeedPath = __DIR__ . '/resources/YouTube/' . str_replace([':', '/', '?', '='], '_', $url) . '.xml';
+
+        // Disable deprecation warnings.
+        // Dice uses ReflectionParameter::getClass(), which is deprecated in PHP 8.
+        error_reporting(E_ALL & ~E_DEPRECATED);
+
+        $mock = new MockHandler([
+            new Response(200, ['Content-Type' => 'application/rss+xml'], file_get_contents($cachedFeedPath)),
+        ]);
+        $stack = HandlerStack::create($mock);
+        $httpClient = new Client(['handler' => $stack]);
+
+        $dice = new Dice();
+        $dice->addRule('*', [
+            'substitutions' => [
+                WebClient::class => [
+                    'instance' => function() use ($dice, $httpClient) {
+                        $wc = new class($dice->create(Logger::class), $httpClient) extends WebClient {
+                            /** @var Client */
+                            private $httpClient;
+
+                            public function __construct($logger, $httpClient) {
+                                parent::__construct(new Configuration, $logger);
+
+                                $this->httpClient = $httpClient;
+                            }
+
+                            public function getHttpClient() {
+                                return $this->httpClient;
+                            }
+                        };
+
+                        return $wc;
+                    }
+                ],
+            ],
+        ]);
+
+        $yt = $dice->create(youtube::class);
+
+        $params = [
+            'channel' => $url,
+        ];
+
+        $yt->load($params);
+
+        // Uncomment the following line to refresh the resources:
+        // file_put_contents($cachedFeedPath, file_get_contents($yt->getXmlUrl($params)));
+
+        $this->assertEquals($feedTitle, $yt->getSpoutTitle());
+        $this->assertEquals($firstItemTitle, $yt->getTitle());
+    }
+
+    public function dataProvider() {
+        return [
+            [
+                'url' => 'https://www.youtube.com/user/ZoggFromBetelgeuse',
+                'feedTitle' => 'Zogg from Betelgeuse',
+                'firstItemTitle' => 'Earthlings 101 - Channel Ad',
+            ],
+            [
+                'url' => 'https://www.youtube.com/channel/UCKY00CSQo1MoC27bdGd-w_g',
+                'feedTitle' => 'Zogg from Betelgeuse',
+                'firstItemTitle' => 'Earthlings 101 - Channel Ad',
+            ],
+            [
+                'url' => 'https://www.youtube.com/ZoggFromBetelgeuse',
+                'feedTitle' => 'Zogg from Betelgeuse',
+                'firstItemTitle' => 'Earthlings 101 - Channel Ad',
+            ],
+            [
+                'url' => 'ZoggFromBetelgeuse',
+                'feedTitle' => 'Zogg from Betelgeuse',
+                'firstItemTitle' => 'Earthlings 101 - Channel Ad',
+            ],
+        ];
+    }
+}
