0c9099ecebbe89c023c50f5eb9a79104aba5c8c5
Jan Tojnar <jtojnar@gmail.com>
2021-10-10 23:18:13 +0200

    tooling: Modernize dist script
    - Use pathlib.Path for paths
    - Move body to main

--- utils/create-zipball.py
+++ utils/create-zipball.py
@@ -2,12 +2,11 @@
 import json
 import logging
 import os
-import os.path
-import pathlib
 import re
 import subprocess
 import tempfile
 import zipfile
+from pathlib import Path
 
 logger = logging.getLogger('create-zipfile')
 
@@ -44,12 +43,12 @@ DISALLOWED_DEST_PATTERNS = list(map(re.compile, [
     r'^vendor/smalot/pdfparser/src/Smalot/PdfParser/Tests',
 ]))
 
-def is_not_unimportant(dest):
-    filename = os.path.basename(dest)
+def is_not_unimportant(dest: Path) -> bool:
+    filename = dest.name
 
     filename_disallowed = any([expr.match(filename) for expr in DISALLOWED_FILENAME_PATTERNS])
 
-    dest_disallowed = any([expr.match(dest) for expr in DISALLOWED_DEST_PATTERNS])
+    dest_disallowed = any([expr.match(str(dest)) for expr in DISALLOWED_DEST_PATTERNS])
 
     allowed = not (filename_disallowed or dest_disallowed)
 
@@ -61,85 +60,91 @@ class ZipFile(zipfile.ZipFile):
             allowed = lambda item: True
 
         for root, dirs, files in os.walk(name):
+            root = Path(root)
 
             for directory in dirs:
-                path = os.path.join(root, directory)
+                directory = Path(directory)
+                path = root / directory
 
                 if allowed(path):
                     # Directories are empty files whose path ends with a slash.
                     # https://mail.python.org/pipermail/python-list/2003-June/205859.html
-                    archive.writestr(os.path.join(self.prefix, path) + '/', '')
+                    self.writestr(str(self.prefix / path) + '/', '')
 
             for file in files:
-                path = os.path.join(root, file)
+                path = root / file
 
                 if allowed(path):
-                    self.write(path, os.path.join(self.prefix, path))
+                    self.write(path, self.prefix / path)
     def file(self, name):
-        self.write(name, os.path.join(self.prefix, name))
+        self.write(name, self.prefix / name)
 
-source_dir = os.getcwd()
-with tempfile.TemporaryDirectory(prefix='selfoss-dist-') as temp_dir:
-    dirty = subprocess.run(['git','-C', source_dir, 'diff-index', '--quiet', 'HEAD']).returncode == 1
-    if dirty:
-        logger.warning('Repository contains uncommitted changes that will not be included in the dist archive.')
+def main():
+    source_dir = Path.cwd()
+    with tempfile.TemporaryDirectory(prefix='selfoss-dist-') as temp_dir:
+        dirty = subprocess.run(['git','-C', source_dir, 'diff-index', '--quiet', 'HEAD']).returncode == 1
+        if dirty:
+            logger.warning('Repository contains uncommitted changes that will not be included in the dist archive.')
 
-    logger.info('Cloning the repository into a temporary directory…')
-    subprocess.check_call(['git', 'clone', '--shared', source_dir, temp_dir])
+        logger.info('Cloning the repository into a temporary directory…')
+        subprocess.check_call(['git', 'clone', '--shared', source_dir, temp_dir])
 
-    os.chdir(temp_dir)
+        os.chdir(temp_dir)
 
-    with open('package.json', encoding='utf-8') as package_json:
-        pkg = json.load(package_json)
+        with open('package.json', encoding='utf-8') as package_json:
+            pkg = json.load(package_json)
 
-    version = pkg['ver']
+        version = pkg['ver']
 
-    # Tagged releases will be bumped by a developer to version not including -SNAPSHOT suffix and we do not need to include the commit.
-    if 'SNAPSHOT' in version:
-        logger.info('Inserting commit hash into version numbers…')
-        short_commit = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], encoding='utf-8').strip()
-        version = version.replace('SNAPSHOT', short_commit)
-        subprocess.check_call(['npm', 'run', 'bump-version', version])
+        # Tagged releases will be bumped by a developer to version not including -SNAPSHOT suffix and we do not need to include the commit.
+        if 'SNAPSHOT' in version:
+            logger.info('Inserting commit hash into version numbers…')
+            short_commit = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], encoding='utf-8').strip()
+            version = version.replace('SNAPSHOT', short_commit)
+            subprocess.check_call(['npm', 'run', 'bump-version', version])
 
-    logger.info('Installing dependencies…')
-    subprocess.check_call(['npm', 'run', 'install-dependencies'])
+        logger.info('Installing dependencies…')
+        subprocess.check_call(['npm', 'run', 'install-dependencies'])
 
-    logger.info('Building asset bundles…')
-    subprocess.check_call(['npm', 'run', 'build'])
+        logger.info('Building asset bundles…')
+        subprocess.check_call(['npm', 'run', 'build'])
 
-    logger.info('Generating config-example.ini…')
-    subprocess.check_call(['php', 'utils/generate-config-example.php'])
+        logger.info('Generating config-example.ini…')
+        subprocess.check_call(['php', 'utils/generate-config-example.php'])
 
-    logger.info('Optimizing PHP dependencies…')
-    subprocess.check_call(['composer', 'install', '--no-dev', '--optimize-autoloader'])
+        logger.info('Optimizing PHP dependencies…')
+        subprocess.check_call(['composer', 'install', '--no-dev', '--optimize-autoloader'])
 
-    filename = 'selfoss-{}.zip'.format(version)
+        filename = 'selfoss-{}.zip'.format(version)
 
-    # fill archive with data
-    with ZipFile(os.path.join(source_dir, filename), 'w', zipfile.ZIP_DEFLATED) as archive:
-        archive.prefix = 'selfoss'
+        # fill archive with data
+        with ZipFile(source_dir / filename, 'w', zipfile.ZIP_DEFLATED) as archive:
+            archive.prefix = Path('selfoss')
 
-        # we only care for locale assets now, since those are still used by backend code
-        archive.directory('assets/locale/')
+            # we only care for locale assets now, since those are still used by backend code
+            archive.directory('assets/locale/')
 
-        archive.directory('src/')
-        archive.directory('vendor/', is_not_unimportant)
+            archive.directory('src/')
+            archive.directory('vendor/', is_not_unimportant)
 
-        # pack all bundles and bundled assets
-        archive.directory('public/')
+            # pack all bundles and bundled assets
+            archive.directory('public/')
 
-        # copy data: only directory structure and .htaccess for deny
-        archive.directory('data/', lambda file: pathlib.Path(file).is_dir())
-        archive.file('data/cache/.htaccess')
-        archive.file('data/logs/.htaccess')
-        archive.file('data/sqlite/.htaccess')
-        archive.directory('data/fulltextrss')
+            # copy data: only directory structure and .htaccess for deny
+            archive.directory('data/', lambda file: file.is_dir())
+            archive.file('data/cache/.htaccess')
+            archive.file('data/logs/.htaccess')
+            archive.file('data/sqlite/.htaccess')
+            archive.directory('data/fulltextrss')
 
-        archive.file('.htaccess')
-        archive.file('README.md')
-        archive.file('config-example.ini')
-        archive.file('index.php')
-        archive.file('run.php')
-        archive.file('cliupdate.php')
+            archive.file('.htaccess')
+            archive.file('README.md')
+            archive.file('config-example.ini')
+            archive.file('index.php')
+            archive.file('run.php')
+            archive.file('cliupdate.php')
 
-        logger.info('Zipball ‘{}’ was successfully generated.'.format(filename))
+            logger.info('Zipball ‘{}’ was successfully generated.'.format(filename))
+
+if __name__ == '__main__':
+    main()
