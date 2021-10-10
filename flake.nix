807b61dbed6aa702919b2648b3a628a9d5b6b1e5
Jan Tojnar <jtojnar@gmail.com>
2021-10-10 22:01:54 +0200

    flake: format with nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -24,51 +24,52 @@
       # By default, we use the default PHP version from Nixpkgs.
       matrix.phpPackage = "php";
     in
-      # For each supported platform,
-      utils.lib.eachDefaultSystem (system:
-        let
-          # Get Nixpkgs packages for current platform.
-          pkgs = nixpkgs.legacyPackages.${system};
+    # For each supported platform,
+    utils.lib.eachDefaultSystem (system:
+      let
+        # Get Nixpkgs packages for current platform.
+        pkgs = nixpkgs.legacyPackages.${system};
 
-          # Create a PHP package from the selected PHP package, with some extra extensions enabled.
-          php = phps.packages.${system}.${matrix.phpPackage}.withExtensions ({ enabled, all }: with all; enabled ++ [
-            imagick
-          ]);
+        # Create a PHP package from the selected PHP package, with some extra extensions enabled.
+        php = phps.packages.${system}.${matrix.phpPackage}.withExtensions ({ enabled, all }: with all; enabled ++ [
+          imagick
+        ]);
 
-          # Create a Python package with some extra packages installed.
-          python = pkgs.python3.withPackages (pp: with pp; [
-            # For integration tests.
-            bcrypt
-            requests
-          ]);
-        in {
-          # Expose shell environment for development.
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = [
-              # Composer and PHP for back-end.
-              php
-              php.packages.composer
+        # Create a Python package with some extra packages installed.
+        python = pkgs.python3.withPackages (pp: with pp; [
+          # For integration tests.
+          bcrypt
+          requests
+        ]);
+      in
+      {
+        # Expose shell environment for development.
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            # Composer and PHP for back-end.
+            php
+            php.packages.composer
 
-              # Back-end code validation.
-              php.packages.psalm
-              php.packages.phpstan
+            # Back-end code validation.
+            php.packages.psalm
+            php.packages.phpstan
 
-              # npm for front-end.
-              pkgs.nodejs_latest
+            # npm for front-end.
+            pkgs.nodejs_latest
 
-              # For building zip archive.
-              pkgs.jq
+            # For building zip archive.
+            pkgs.jq
 
-              # For building zip archive and integration tests.
-              python
+            # For building zip archive and integration tests.
+            python
 
-              # Website generator.
-              pkgs.zola
-            ];
+            # Website generator.
+            pkgs.zola
+          ];
 
-            # node-gyp wants some locales, let’s make them available through an environment variable.
-            LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-          };
-        }
-      );
+          # node-gyp wants some locales, let’s make them available through an environment variable.
+          LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+        };
+      }
+    );
 }
