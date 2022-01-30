ee3c9be8b5a6a6cc65edba6a805eb0622a54150d
Jan Tojnar <jtojnar@gmail.com>
2022-01-30 18:58:15 +0100

    tests: ignore deprecation triggered by Dice
    error_reporting is insufficient with PHP 8.1.

--- tests/Spouts/YouTubeTest.php
+++ tests/Spouts/YouTubeTest.php
@@ -23,42 +23,52 @@ final class YouTubeTest extends TestCase {
         $cachedFeedPath = __DIR__ . '/resources/YouTube/' . str_replace([':', '/', '?', '='], '_', $url) . '.xml';
 
         // Disable deprecation warnings.
-        // Dice uses ReflectionParameter::getClass(), which is deprecated in PHP 8.
-        error_reporting(E_ALL & ~E_DEPRECATED);
+        set_error_handler(function($errno, $errstr, $errfile, $errline) {
+            if (preg_match('(Method ReflectionParameter::getClass\(\) is deprecated)', $errstr)) {
+                // Dice uses ReflectionParameter::getClass(), which is deprecated in PHP 8.
+                return true;
+            }
 
-        $mock = new MockHandler([
-            new Response(200, ['Content-Type' => 'application/rss+xml'], file_get_contents($cachedFeedPath)),
-        ]);
-        $stack = HandlerStack::create($mock);
-        $httpClient = new Client(['handler' => $stack]);
+            return false;
+        }, E_DEPRECATED);
 
-        $dice = new Dice();
-        $dice->addRule('*', [
-            'substitutions' => [
-                WebClient::class => [
-                    'instance' => function() use ($httpClient) {
-                        $stub = $this->createMock(WebClient::class);
-                        $stub->method('getHttpClient')->willReturn($httpClient);
+        try {
+            $mock = new MockHandler([
+                new Response(200, ['Content-Type' => 'application/rss+xml'], file_get_contents($cachedFeedPath)),
+            ]);
+            $stack = HandlerStack::create($mock);
+            $httpClient = new Client(['handler' => $stack]);
 
-                        return $stub;
-                    },
+            $dice = new Dice();
+            $dice->addRule('*', [
+                'substitutions' => [
+                    WebClient::class => [
+                        'instance' => function() use ($httpClient) {
+                            $stub = $this->createMock(WebClient::class);
+                            $stub->method('getHttpClient')->willReturn($httpClient);
+
+                            return $stub;
+                        },
+                    ],
                 ],
-            ],
-        ]);
+            ]);
 
-        $yt = $dice->create(youtube::class);
+            $yt = $dice->create(youtube::class);
 
-        $params = [
-            'channel' => $url,
-        ];
+            $params = [
+                'channel' => $url,
+            ];
 
-        $yt->load($params);
+            $yt->load($params);
 
-        // Uncomment the following line to refresh the resources:
-        // file_put_contents($cachedFeedPath, file_get_contents($yt->getXmlUrl($params)));
+            // Uncomment the following line to refresh the resources:
+            // file_put_contents($cachedFeedPath, file_get_contents($yt->getXmlUrl($params)));
 
-        $this->assertEquals($feedTitle, $yt->getSpoutTitle());
-        $this->assertEquals($firstItemTitle, $yt->getTitle());
+            $this->assertEquals($feedTitle, $yt->getSpoutTitle());
+            $this->assertEquals($firstItemTitle, $yt->getTitle());
+        } finally {
+            restore_error_handler();
+        }
     }
 
     public function dataProvider() {
