746ff568d835905f8074a6cf0c080440d35bddaf
Jan Tojnar <jtojnar@gmail.com>
2022-10-07 06:26:46 +0200

    client: Escape error messages in OPML import
    This will come in especially handy with the next commit, which will pass along messages from the XML parser.
    
    Ideally, we would merge the import page with the rest of the APP and have React handle the escaping but we are not there yet.

--- assets/js/opml.js
+++ assets/js/opml.js
@@ -2,6 +2,14 @@ const form = document.querySelector('form');
 const msgContainer = document.querySelector('.message-container');
 const submit = document.querySelector('input[type=submit]');
 
+function escapeHtml(text) {
+    return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+}
+
+function listMessages(messages) {
+    return (messages ?? []).map(escapeHtml).join('<br>');
+}
+
 form.addEventListener('submit', e => {
     e.preventDefault();
 
@@ -19,17 +27,17 @@ form.addEventListener('submit', e => {
     fetch(request).then(response => {
         return response.json().then(({messages}) => {
             if (response.status === 200) {
-                msgContainer.innerHTML = `<p class="msg success">${messages.join('<br>')} You might want to <a href="update">update now</a> or <a href="./">view your feeds</a>.</p>`;
+                msgContainer.innerHTML = `<p class="msg success">${listMessages(messages)} You might want to <a href="update">update now</a> or <a href="./">view your feeds</a>.</p>`;
             } else if (response.status === 202) {
-                msgContainer.innerHTML = `<p class="msg error">The following feeds could not be imported:<br>${messages.join('<br>')}</p>`;
+                msgContainer.innerHTML = `<p class="msg error">The following feeds could not be imported:<br>${listMessages(messages)}</p>`;
             } else if (response.status === 400) {
-                msgContainer.innerHTML = `<p class="msg error">There was a problem importing your OPML file:<br>${messages.join('<br>')}</p>`;
+                msgContainer.innerHTML = `<p class="msg error">There was a problem importing your OPML file:<br>${listMessages(messages)}</p>`;
             } else {
-                msgContainer.innerHTML = `<p class="msg error">Unexpected happened. <details><pre>${JSON.stringify(messages)}</pre></details></p>`;
+                msgContainer.innerHTML = `<p class="msg error">Unexpected happened. <details><pre>${escapeHtml(JSON.stringify(messages))}</pre></details></p>`;
             }
         });
     }).catch(err => {
-        msgContainer.innerHTML = `<p class="msg error">Unexpected happened. <details><pre>${JSON.stringify(err)}</pre></details></p>`;
+        msgContainer.innerHTML = `<p class="msg error">Unexpected happened. <details><pre>${escapeHtml(JSON.stringify(err))}</pre></details></p>`;
     }).finally(() => {
         submit.disabled = false;
         submit.value = originalButtonCaption;
