fossar/selfoss

commit f3b279c808e8a5d7e3ab5c8254bdda83c094b50e

client: Port entries to React

assets/js/templates/EntriesPage.jsx
@@ -1,0 +1,205 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Item from './Item';
+import * as sourceRequests from '../requests/sources';
+import { LoadingState } from '../requests/LoadingState';
+import Spinner from './Spinner';
+import classNames from 'classnames';
+
+
+// updates a source
+function handleRefreshSource({ event, setLoadingState }) {
+    event.preventDefault();
+
+    // show loading
+    setLoadingState(LoadingState.LOADING);
+
+    sourceRequests.refreshSingle(selfoss.filter.source).then(() => {
+        // hide nav on smartphone
+        if (selfoss.isSmartphone()) {
+            $('#nav-mobile-settings').click();
+        }
+
+        // refresh list
+        // Will also clear the loading status.
+        selfoss.db.reloadList();
+    }).catch((error) => {
+        alert(selfoss.ui._('error_refreshing_source') + ' ' + error.message);
+    });
+}
+
+function loadMore({ event, entries }) {
+    event.preventDefault();
+
+    const lastEntry = entries[entries.length - 1];
+    selfoss.events.setHash();
+    selfoss.filter.update({
+        extraIds: [],
+        fromDatetime: new Date(lastEntry.datetime),
+        fromId: lastEntry.id
+    });
+
+    selfoss.db.reloadList(true);
+}
+
+export function EntriesPage({ entries, hasMore, loadingState, setLoadingState, selectedEntry, expandedEntries }) {
+    const firstPage = typeof selfoss.filter.fromId === 'undefined' && typeof selfoss.filter.fromDatetime === 'undefined';
+    const allowedToUpdate = !selfoss.config.authEnabled || selfoss.config.allowPublicUpdate || selfoss.loggedin.value;
+
+    // TODO: make this update when it changes
+    const isOnline = selfoss.db.online;
+
+    return (
+        <React.Fragment>
+            {loadingState === LoadingState.LOADING ? <Spinner /> : null}
+            {selfoss.filter.source && allowedToUpdate && firstPage && isOnline ?
+                <button
+                    type="button"
+                    className="refresh-source"
+                    onClick={(event) => handleRefreshSource({ event, setLoadingState })}
+                >
+                    {selfoss.ui._('source_refresh')}
+                </button>
+                : null
+            }
+            {entries.map((entry) => (
+                <Item
+                    key={entry.id}
+                    item={entry}
+                    selected={selectedEntry == entry.id}
+                    expanded={expandedEntries[entry.id] ?? false}
+                />
+            ))}
+            <div id="stream-buttons">
+                {entries.length === 0 ?
+                    <p aria-live="assertive" className="stream-empty">{selfoss.ui._('no_entries')}</p>
+                    : null}
+                {hasMore ?
+                    <button
+                        className={classNames({'stream-button': true, 'stream-more': true})}
+                        accessKey="m"
+                        aria-label={selfoss.ui._('more')}
+                        onClick={(event) => loadMore({ event, entries })}
+                    >
+                        <span>{selfoss.ui._('more')}</span>
+                    </button>
+                    : null}
+                {entries.length > 0 ?
+                    <button
+                        className="stream-button mark-these-read"
+                        aria-label={selfoss.ui._('markread')}
+                        onClick={selfoss.markVisibleRead}
+                    >
+                        <span>{selfoss.ui._('markread')}</span>
+                    </button>
+                    : null
+                }
+                {loadingState == LoadingState.FAILURE ?
+                    <button
+                        className="stream-button stream-error"
+                        aria-live="assertive"
+                        aria-label={selfoss.ui._('streamerror')}
+                        onClick={selfoss.db.reloadList}
+                    >
+                        {selfoss.ui._('streamerror')}
+                    </button>
+                    : null}
+            </div>
+        </React.Fragment>
+    );
+}
+
+export class StateHolder extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            entries: [],
+            hasMore: false,
+            /**
+             * Currently selected entry.
+             * The id in the location.hash should imply the selected entry.
+             * It will also be used for keyboard navigation (for finding previous/next).
+             */
+            selectedEntry: null,
+            expandedEntries: {},
+            loadingState: LoadingState.INITIAL
+        };
+    }
+
+    setEntries(entries) {
+        if (typeof entries === 'function') {
+            this.setState({ entries: entries(this.state.entries) });
+        } else {
+            this.setState({ entries });
+        }
+    }
+
+    appendEntries(extraEntries) {
+        this.setEntries((entries) => [...entries, ...extraEntries]);
+    }
+
+    setSelectedEntry(selectedEntry) {
+        if (typeof selectedEntry === 'function') {
+            this.setState({ selectedEntry: selectedEntry(this.state.selectedEntry) });
+        } else {
+            this.setState({ selectedEntry });
+        }
+    }
+
+    setExpandedEntries(expandedEntries) {
+        if (typeof expandedEntries === 'function') {
+            this.setState({
+                expandedEntries: expandedEntries(this.state.expandedEntries)
+            });
+        } else {
+            this.setState({ expandedEntries });
+        }
+    }
+
+    setEntryExpanded(id, expand) {
+        if (typeof expand === 'function') {
+            this.setExpandedEntries((oldEntries) => ({
+                ...oldEntries,
+                [id]: expand(oldEntries[id] ?? false)
+            }));
+        } else {
+            this.setExpandedEntries((oldEntries) => ({
+                ...oldEntries,
+                [id]: expand
+            }));
+        }
+    }
+
+    setHasMore(hasMore) {
+        if (typeof hasMore === 'function') {
+            this.setState({ hasMore: hasMore(this.state.hasMore) });
+        } else {
+            this.setState({ hasMore });
+        }
+    }
+
+    setLoadingState(loadingState) {
+        if (typeof loadingState === 'function') {
+            this.setState({ loadingState: loadingState(this.state.loadingState) });
+        } else {
+            this.setState({ loadingState });
+        }
+    }
+
+    render() {
+        return (
+            <EntriesPage
+                entries={this.state.entries}
+                selectedEntry={this.state.selectedEntry}
+                expandedEntries={this.state.expandedEntries}
+                hasMore={this.state.hasMore}
+                loadingState={this.state.loadingState}
+                setLoadingState={this.setLoadingState.bind(this)}
+            />
+        );
+    }
+}
+
+export function anchor(element) {
+    return ReactDOM.render(<StateHolder />, element);
+}

assets/js/index.js
@@ -1,6 +1,5 @@
 import 'regenerator-runtime/runtime';
 import './jquery';
-import './lazy-image-loader';
 import 'spectrum-colorpicker';
 import 'jquery-hotkeys';
 import selfoss from './selfoss-base';
@@ -11,8 +10,6 @@ import './selfoss-db';
 import './selfoss-ui';
 import './selfoss-events';
 import './selfoss-events-navigation';
-import './selfoss-events-entries';
-import './selfoss-events-entriestoolbar';
 import './selfoss-shortcuts';
 import '@fancyapps/fancybox';
 

assets/js/lazy-image-loader.js
@@ -1,12 +1,0 @@
-import jQuery from 'jquery';
-
-/**
- * load images on showing an message entry
- */
-(function($) {
-    $.fn.lazyLoadImages = function() {
-        $(this).find('img').each(function(i, self) {
-            $(self).attr('src', $(self).attr('data-selfoss-src'));
-        });
-    };
-})(jQuery);

assets/js/selfoss-base.js
@@ -90,6 +90,11 @@ var selfoss = {
     htmlTitle: 'selfoss',
 
     /**
+     * React component for entries page.
+     */
+    entriesPage: null,
+
+    /**
      * React component for sources page.
      */
     sourcesPage: null,
@@ -421,10 +426,9 @@ var selfoss = {
     anonymize: function(parent) {
         var anonymizer = selfoss.config.anonymizer;
         if (anonymizer !== null) {
-            parent.find('a').each(function(i, link) {
-                link = $(link);
-                if (typeof link.attr('href') != 'undefined' && link.attr('href').indexOf(anonymizer) != 0) {
-                    link.attr('href', anonymizer + link.attr('href'));
+            parent.querySelectorAll('a').forEach((link) => {
+                if (typeof link.getAttribute('href') !== 'undefined' && !link.getAttribute('href').startsWith(anonymizer)) {
+                    link.setAttribute('href', anonymizer + link.getAttribute('href'));
                 }
             });
         }
@@ -457,14 +461,18 @@ var selfoss = {
      * Mark all visible items as read
      */
     markVisibleRead: function() {
-        var ids = [];
-        var tagUnreadDiff = {};
-        var sourceUnreadDiff = [];
-        $('.entry.unread').each(function(index, item) {
-            ids.push($(item).attr('data-entry-id'));
+        let ids = [];
+        let tagUnreadDiff = {};
+        let sourceUnreadDiff = [];
 
-            $('.entry-tags-tag', item).each(function(index, tagEl) {
-                var tag = $(tagEl).html();
+        let markedEntries = selfoss.entriesPage.state.entries.map((entry) => {
+            if (!entry.unread) {
+                return entry;
+            }
+
+            ids.push(entry.id);
+
+            Object.keys(entry.tags).forEach((tag) => {
                 if (Object.keys(tagUnreadDiff).includes(tag)) {
                     tagUnreadDiff[tag] += -1;
                 } else {
@@ -472,59 +480,40 @@ var selfoss = {
                 }
             });
 
-            const source = $(item).data('entry-source');
+            const { source } = entry;
             if (Object.keys(sourceUnreadDiff).includes(source)) {
                 sourceUnreadDiff[source] += -1;
             } else {
                 sourceUnreadDiff[source] = -1;
             }
+
+            return {
+                ...entry,
+                unread: false
+            };
         });
+        const oldEntries = selfoss.entriesPage.state.entries;
+        const hadMore = selfoss.entriesPage.state.hasMore;
 
         // close opened entry and list
         selfoss.filterReset({}, true);
 
-        if (ids.length === 0 && selfoss.filter.type === FilterType.UNREAD) {
-            $('.entry').remove();
-            if (selfoss.unreadItemsCount.value > 0) {
-                selfoss.db.reloadList();
-            } else {
-                selfoss.ui.refreshStreamButtons(true);
-            }
+        if (ids.length !== 0 && selfoss.filter.type === FilterType.UNREAD) {
+            markedEntries = markedEntries.filter(({ id }) => ids.includes(id));
         }
 
-        if (ids.length === 0) {
-            return;
-        }
-
-        var content = $('#content');
-        var articleList = content.html();
-        var hadMore = $('.stream-more').is(':visible');
-
-        selfoss.ui.beforeReloadList();
+        selfoss.entriesPage.setLoadingState(LoadingState.LOADING);
+        selfoss.entriesPage.setEntries(markedEntries);
 
         const unreadstats = selfoss.unreadItemsCount.value - ids.length;
-        var displayed = false;
-        var displayNextUnread = function() {
-            if (!displayed) {
-                displayed = true;
-                selfoss.refreshUnread(unreadstats);
-                selfoss.ui.refreshTagSourceUnread(tagUnreadDiff,
-                    sourceUnreadDiff);
-
-                selfoss.ui.hideMobileNav();
-
-                selfoss.db.reloadList(false, false);
-            }
-        };
 
         if (selfoss.db.enableOffline) {
             selfoss.refreshUnread(unreadstats);
-            selfoss.dbOffline.entriesMark(ids, false).then(displayNextUnread);
+            selfoss.dbOffline.entriesMark(ids, false);
         }
 
         itemsRequests.markAll(ids).then(function() {
-            selfoss.db.setOnline();
-            displayNextUnread();
+            selfoss.entriesPage.setLoadingState(LoadingState.SUCCESS);
         }).catch(function(error) {
             selfoss.handleAjaxError(error).then(function() {
                 let statuses = ids.map(id => ({
@@ -534,9 +523,9 @@ var selfoss = {
                 }));
                 selfoss.dbOffline.enqueueStatuses(statuses);
             }).catch(function(error) {
-                content.html(articleList);
-                selfoss.ui.refreshStreamButtons(true, hadMore);
-                selfoss.ui.listReady();
+                selfoss.entriesPage.setLoadingState(LoadingState.SUCCESS);
+                selfoss.entriesPage.setEntries(oldEntries);
+                selfoss.entriesPage.setHasMore(hadMore);
                 selfoss.ui.showError(selfoss.ui._('error_mark_items') + ' ' + error.message);
             });
         });

assets/js/selfoss-db-offline.js
@@ -115,7 +115,6 @@ selfoss.dbOffline = {
                 });
 
                 selfoss.ui.setOnline();
-                $('#content').addClass('loading');
                 selfoss.db.tryOnline()
                     .then(function() {
                         selfoss.reloadTags();

assets/js/selfoss-db-online.js
@@ -1,7 +1,6 @@
-import React from 'jsx-dom';
 import selfoss from './selfoss-base';
 import * as itemsRequests from './requests/items';
-import Item from './templates/Item';
+import { LoadingState } from './requests/LoadingState';
 import { FilterType } from './Filter';
 
 selfoss.dbOnline = {
@@ -187,8 +186,8 @@ selfoss.dbOnline = {
             }
 
             if ('stats' in data && data.stats.unread > 0 &&
-                ($('.stream-empty').is(':visible') ||
-                $('.stream-error').is(':visible'))) {
+                selfoss.entriesPage && (selfoss.entriesPage.state.entries.length === 0 ||
+                selfoss.entriesPage.state.entries.loadingState === LoadingState.FAILURE)) {
                 selfoss.db.reloadList();
             } else {
                 if ('itemUpdates' in data) {
@@ -202,8 +201,8 @@ selfoss.dbOnline = {
                     } else {
                         unreadCount = selfoss.unreadItemsCount.value;
                     }
-                    if (unreadCount > $('.entry.unread').length) {
-                        $('.stream-more').show();
+                    if (selfoss.entriesPage && unreadCount > selfoss.entriesPage.state.entries.filter(({ unread }) => unread == 1).length) {
+                        selfoss.entriesPage.setHasMore(true);
                     }
                 }
             }

assets/js/selfoss-db.js
@@ -9,11 +9,10 @@
  * dbOffline is the entry point for the offline database held in the client.
  */
 
-import React from 'jsx-dom';
 import selfoss from './selfoss-base';
 import { OfflineStorageNotAvailableError } from './errors';
 import { FilterType } from './Filter';
-import Item from './templates/Item';
+import { LoadingState } from './requests/LoadingState';
 
 selfoss.db = {
 
@@ -120,7 +119,7 @@ selfoss.db = {
             selfoss.dbOffline.olderEntriesOnline = false;
         }
 
-        selfoss.ui.beforeReloadList(!append);
+        selfoss.entriesPage?.setLoadingState(LoadingState.LOADING);
 
         var reload = function() {
             let reloader = selfoss.dbOffline.reloadList;
@@ -136,31 +135,40 @@ selfoss.db = {
                 reloader = selfoss.dbOnline.reloadList;
             }
 
+            selfoss.entriesPage?.setLoadingState(LoadingState.LOADING);
             reloader().then(({ entries, hasMore }) => {
-                const firstPage = typeof selfoss.filter.fromId === 'undefined' && typeof selfoss.filter.fromDatetime === 'undefined';
-                const allowedToUpdate = !selfoss.config.authEnabled || selfoss.config.allowPublicUpdate || selfoss.loggedin.value;
+                selfoss.entriesPage.setLoadingState(LoadingState.SUCCESS);
+                selfoss.entriesPage.setHasMore(hasMore);
 
-                let content = $('#content');
-                let newContent = content.clone().empty();
-                if (selfoss.filter.source && allowedToUpdate && firstPage && reloader === selfoss.dbOnline.reloadList) {
-                    newContent.append(<button type="button" id="refresh-source" class="refresh-source">{selfoss.ui._('source_refresh')}</button>);
-                }
-                newContent.append(entries.map(entry => <Item item={entry} />));
-
-                if (!firstPage) {
-                    content.append(newContent.children());
+                if (append) {
+                    selfoss.entriesPage.appendEntries(entries);
                 } else {
-                    content.replaceWith(newContent);
+                    selfoss.entriesPage.setExpandedEntries({});
+                    selfoss.entriesPage.setEntries(entries);
                 }
 
-                selfoss.ui.refreshStreamButtons(true, hasMore);
+                // open selected entry only if entry was requested (i.e. if not streaming
+                // more)
+                if (selfoss.events.entryId && selfoss.filter.fromId === undefined) {
+                    var entry = document.querySelector(`.entry[data-entry-id="${selfoss.events.entryId}"]`);
 
-                selfoss.ui.afterReloadList(!append);
+                    if (!entry) {
+                        return;
+                    }
+
+                    selfoss.ui.entryActivate(selfoss.events.entryId);
+                    // ensure scrolling to requested entry even if scrolling to article
+                    // header is disabled
+                    if (!selfoss.config.scrollToArticleHeader) {
+                        // needs to be delayed for some reason
+                        requestAnimationFrame(function() {
+                            entry.scrollIntoView();
+                        });
+                    }
+                }
             }).catch(function(error) {
+                selfoss.entriesPage.setLoadingState(LoadingState.FAILURE);
                 selfoss.ui.showError(selfoss.ui._('error_loading') + ' ' + error.message);
-                selfoss.events.entries();
-                selfoss.ui.refreshStreamButtons();
-                $('.stream-error').show();
             });
         };
 

assets/js/selfoss-events-entries.js
@@ -1,243 +1,0 @@
-import { createFocusTrap } from 'focus-trap';
-import selfoss from './selfoss-base';
-import * as sourceRequests from './requests/sources';
-
-/**
- * initialize events for entries
- */
-selfoss.events.entries = function() {
-
-    $('.entry, .entry-title').unbind('click');
-
-    // show/hide entry
-    var target = selfoss.isMobile() ? '.entry' : '.entry-title';
-    $(target).click(function() {
-        var parent = ((target == '.entry') ? $(this) : $(this).parent());
-
-        // prevent event on fullscreen touch
-        if (document.body.classList.contains('fullscreen-mode')) {
-            return;
-        }
-
-        var autoMarkAsRead = selfoss.loggedin.value && selfoss.config.autoMarkAsRead && parent.hasClass('unread');
-        var autoHideReadOnMobile = selfoss.config.autoHideReadOnMobile && parent.hasClass('unread');
-
-        // anonymize
-        selfoss.anonymize(parent.find('.entry-content'));
-
-        var entryId = parent.attr('data-entry-id');
-
-        var entryContent = parent.find('.entry-content');
-
-        // show entry in popup
-        if (selfoss.isSmartphone()) {
-            // save scroll position
-            var scrollTop = $(window).scrollTop();
-
-            // hide nav
-            if ($('#nav').is(':visible')) {
-                scrollTop = scrollTop - $('#nav').height();
-                scrollTop = scrollTop < 0 ? 0 : scrollTop;
-                $(window).scrollTop(scrollTop);
-                $('#nav').hide();
-            }
-
-            // show fullscreen
-            document.body.classList.add('fullscreen-mode');
-            selfoss.ui.entrySelect(parent);
-            selfoss.ui.entryExpand(parent);
-            selfoss.events.setHash('same', 'same', entryId);
-            selfoss.events.entriesToolbar(parent);
-
-            parent.attr('aria-modal', 'true');
-
-            if (selfoss.config.loadImagesOnMobile) {
-                parent.find('.entry-loadimages').hide();
-                entryContent.lazyLoadImages();
-            }
-
-            let trap = createFocusTrap(parent.get(0));
-            trap.activate();
-
-            // set events for closing fullscreen
-            var closeTargets = $().add(parent).add(parent.find('.entry-close'));
-            var parentNative = parent.get(0);
-            parentNative.closeFullScreen = function(e) {
-                // can be called by other things
-                if (e) {
-                    e.stopPropagation();
-
-                    // do not exit fullscreen when clicking link
-                    if (e.target.tagName.toLowerCase() == 'a') {
-                        return;
-                    }
-                }
-
-                document.body.classList.remove('fullscreen-mode');
-
-                selfoss.ui.entryCollapse(parent);
-                selfoss.events.setHash();
-
-                if (autoHideReadOnMobile && (parent.hasClass('unread') == false)) {
-                    parent.hide();
-                }
-
-                parent.attr('aria-modal', 'false');
-
-                trap.deactivate();
-
-                closeTargets.off('click', parentNative.closeFullScreen);
-                parentNative.closeFullScreen = null;
-
-                $(window).scrollTop(scrollTop);
-            };
-            closeTargets.click(parentNative.closeFullScreen);
-
-            // automark as read
-            if (autoMarkAsRead) {
-                parent.find('.entry-unread').click();
-            }
-        // open entry content
-        } else {
-            // show/hide (with toolbar)
-            if (selfoss.ui.entryIsExpanded(parent)) {
-                selfoss.ui.entryCollapse(parent);
-                selfoss.events.setHash();
-            } else {
-                if (selfoss.config.autoCollapse) {
-                    selfoss.ui.entryCollapseAll();
-                }
-                selfoss.ui.entrySelect(parent);
-                selfoss.ui.entryExpand(parent);
-                selfoss.events.setHash('same', 'same', entryId);
-                selfoss.events.entriesToolbar(parent);
-
-                // automark as read
-                if (autoMarkAsRead) {
-                    parent.find('.entry-unread').click();
-                }
-
-                // setup fancyBox image viewer
-                selfoss.setupFancyBox(entryContent, parent.attr('data-entry-id'));
-
-                // scroll to article header
-                if (selfoss.config.scrollToArticleHeader) {
-                    parent.get(0).scrollIntoView();
-                }
-
-                // turn of column view if entry is too long
-                if (entryContent.height() > $(window).height()) {
-                    entryContent.addClass('entry-content-nocolumns');
-                }
-            }
-
-            // load images not on mobile devices
-            if (selfoss.isMobile() == false || selfoss.config.loadImagesOnMobile) {
-                entryContent.lazyLoadImages();
-            }
-        }
-    });
-
-    // no source click
-    if (selfoss.isSmartphone()) {
-        $('.entry-icon, .entry-datetime').unbind('click').click(function(e) {
-            e.preventDefault();
-            return false;
-        });
-    }
-
-    // scroll load more
-    $(window).unbind('scroll').scroll(function() {
-        if (!selfoss.config.autoStreamMore ||
-           $('#content').is(':visible') == false) {
-            return;
-        }
-
-        if ($('.stream-more').is(':visible')
-           && $('.stream-more').position().top < $(window).height() + $(window).scrollTop()
-           && $('.stream-more').hasClass('loading') == false) {
-            $('.stream-more').click();
-        }
-    });
-
-    $('.mark-these-read').unbind('click').click(selfoss.markVisibleRead);
-
-    $('.stream-error').unbind('click').click(selfoss.db.reloadList);
-
-    // more
-    $('.stream-more').unbind('click').click(function() {
-        var lastEntry = $('.entry').filter(':last');
-        selfoss.events.setHash();
-        selfoss.filter.update({
-            extraIds: [],
-            fromDatetime: new Date(lastEntry.data('entry-datetime')),
-            fromId: lastEntry.data('entry-id')
-        });
-
-        selfoss.db.reloadList(true);
-    });
-
-    // click a source
-    if (selfoss.isSmartphone() == false) {
-        $('.entry-source').unbind('click').click(function() {
-            var entry = $(this).parents('.entry');
-            selfoss.events.setHash('same',
-                'source-' + entry.attr('data-entry-source'));
-        });
-    }
-
-    // click a tag
-    if (selfoss.isSmartphone() == false) {
-        $('.entry-tags-tag').unbind('click').click(function() {
-            var tag = $(this).html();
-            $('#nav-tags .tag').each(function(index, item) {
-                if ($(item).html() == tag) {
-                    $(item).click();
-                    return false;
-                }
-            });
-        });
-    }
-
-    // updates a source
-    $('#refresh-source').unbind('click').click(function() {
-        // show loading
-        var content = $('#content');
-        var articleList = content.html();
-        $('#content').addClass('loading').html('');
-
-        sourceRequests.refreshSingle(selfoss.filter.source).then(() => {
-            // hide nav on smartphone
-            if (selfoss.isSmartphone()) {
-                $('#nav-mobile-settings').click();
-            }
-
-            // refresh list
-            selfoss.db.reloadList();
-        }).catch((error) => {
-            content.html(articleList);
-            $('#content').removeClass('loading');
-            alert(selfoss.ui._('error_refreshing_source') + ' ' + error.message);
-        });
-    });
-
-    // open selected entry only if entry was requested (i.e. if not streaming
-    // more)
-    if (selfoss.events.entryId && selfoss.filter.fromId === undefined) {
-        var entry = $(`.entry[data-entry-id="${selfoss.events.entryId}"]`);
-
-        if (!entry) {
-            return;
-        }
-
-        selfoss.ui.entryActivate(entry);
-        // ensure scrolling to requested entry even if scrolling to article
-        // header is disabled
-        if (!selfoss.config.scrollToArticleHeader) {
-            // needs to be delayed for some reason
-            requestAnimationFrame(function() {
-                entry.get(0).scrollIntoView();
-            });
-        }
-    }
-};

commit ec544b7ae066050dcdb43e362f38854d4fdb7bea

client: Fix “Mark all as read” button not hiding marked articles
Appears to have been broken by https://github.com/fossar/selfoss/commit/f3b279c808e8a5d7e3ab5c8254bdda83c094b50e.

Fixes: https://github.com/fossar/selfoss/issues/1388

assets/js/templates/EntriesPage.jsx
@@ -706,8 +706,8 @@ export default class StateHolder extends React.Component {
         // close opened entry and list
         this.setExpandedEntries({});
 
-        if (ids.length !== 0 && this.props.match.filter === FilterType.UNREAD) {
-            markedEntries = markedEntries.filter(({ id }) => ids.includes(id));
+        if (ids.length !== 0 && this.props.match.params.filter === FilterType.UNREAD) {
+            markedEntries = markedEntries.filter(({ id }) => !ids.includes(id));
         }
 
         this.setLoadingState(LoadingState.LOADING);