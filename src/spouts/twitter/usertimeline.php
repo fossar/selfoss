fossar/selfoss

commit 2ece113a2150bced612dd2464e55f970d6083b7c

Fix some PHPStan issues
Up to level 5.

src/spouts/twitter/usertimeline.php
@@ -64,6 +64,14 @@ class usertimeline extends \spouts\spout {
         ],
     ];
 
+    private const GROUPED_ENTITY_TYPES = [
+        'hashtags',
+        'symbols',
+        'user_mentions',
+        'urls',
+        'media',
+    ];
+
     /** @var string URL of the source */
     protected $htmlUrl = '';
 
@@ -327,7 +335,8 @@ class usertimeline extends \spouts\spout {
     public static function formatEntities(stdClass $groupedEntities): array {
         $result = [];
 
-        foreach ($groupedEntities as $type => $entities) {
+        foreach (self::GROUPED_ENTITY_TYPES as $type) {
+            $entities = $groupedEntities->{$type};
             foreach ($entities as $entity) {
                 $start = $entity->indices[0];
                 $end = $entity->indices[1];

phpstan.neon
@@ -2,3 +2,13 @@ parameters:
     bootstrapFiles:
         - src/constants.php
         - vendor/bin/.phpunit/phpunit/vendor/autoload.php
+
+    ignoreErrors:
+        # We want exhaustiveness check in spouts\twitter\usertimeline::formatEntities
+        - '(Result of \|\| is always true\.)'
+        # The arguments are provided by SimplePie and we want the value of a later one.
+        - '(Constructor of class helpers\\SimplePieFileGuzzle has an unused parameter \$force_fsockopen\.)'
+
+        # Bad PHPStan typing rules.
+        - '(Parameter #3 \$namespace of method XMLWriter::writeAttributeNs\(\) expects string, null given\.)'
+        - '(Parameter #2 \$algo of function password_hash expects int, string given\.)'

src/helpers/Image.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace helpers;
 
+use Elphin\IcoFileLoader;
 use Elphin\IcoFileLoader\IcoFileService;
 use GuzzleHttp\Psr7\Uri;
 use GuzzleHttp\Psr7\UriResolver;
@@ -21,7 +22,7 @@ class Image {
     public const FORMAT_JPEG = 'jpeg';
     public const FORMAT_PNG = 'png';
 
-    private static $extensions = [
+    private const EXTENSIONS = [
         self::FORMAT_JPEG => 'jpg',
         self::FORMAT_PNG => 'png',
     ];
@@ -62,7 +63,7 @@ class Image {
      * @param self::FORMAT_* $format
      */
     public static function getExtension(string $format): string {
-        return self::$extensions[$format];
+        return self::EXTENSIONS[$format];
     }
 
     /**
@@ -222,6 +223,7 @@ class Image {
             }
 
             if ($image === null) {
+                /** @var ?IcoFileLoader\Icon */ // Type annotation says it cannot return null but that is not the case when the ico file does not contain images.
                 $image = $icon->findBest();
             }
 

src/helpers/Misc.php
@@ -34,7 +34,7 @@ class Misc {
      *
      * @param callable|array-key|array{callable|array-key, self::ORDER_*} ...$transformations
      *
-     * @return callable(mixed, mixed): self::CMP_* comparator
+     * @return callable(mixed, mixed): (self::CMP_*) comparator
      */
     public static function compareBy(...$transformations) {
         if (count($transformations) > 0) {
@@ -50,6 +50,7 @@ class Misc {
                         $comparison = $a[$transformation] <=> $b[$transformation];
                     }
 
+                    /** @var self::CMP_* */ // For PHPStan.
                     $comparison = $order * $comparison;
 
                     if ($comparison !== self::CMP_EQ) {

src/helpers/SpoutLoader.php
@@ -74,7 +74,7 @@ class SpoutLoader {
             $this->spouts = $this->loadClasses(__DIR__ . '/../spouts', spout::class);
 
             // sort spouts by name
-            uasort($this->spouts, ['self', 'compareSpoutsByName']);
+            uasort($this->spouts, [self::class, 'compareSpoutsByName']);
         }
     }
 

src/helpers/WebClient.php
@@ -9,6 +9,7 @@ use Fossar\GuzzleTranscoder\GuzzleTranscoder;
 use GuzzleHttp;
 use GuzzleHttp\HandlerStack;
 use Monolog\Logger;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Helper class for web request
@@ -118,11 +119,11 @@ class WebClient {
      * RedirectMiddleware will need to be enabled for this to work.
      *
      * @param string $url requested URL, to use as a fallback
-     * @param GuzzleHttp\Psr7\Response $response response to inspect
+     * @param ResponseInterface $response response to inspect
      *
      * @return string last URL we were redirected to
      */
-    public static function getEffectiveUrl(string $url, GuzzleHttp\Psr7\Response $response): string {
+    public static function getEffectiveUrl(string $url, ResponseInterface $response): string {
         // Sequence of fetched URLs
         $urlStack = array_merge([$url], $response->getHeader(\GuzzleHttp\RedirectMiddleware::HISTORY_HEADER));
 

commit 49c892283a70d7a7214f14bf8079fe9ae1793033

spouts/Twitter: Fix “Undefined property: stdClass::$media”
This got broken in https://github.com/fossar/selfoss/commit/2ece113a2150bced612dd2464e55f970d6083b7c

src/spouts/twitter/usertimeline.php
@@ -341,6 +341,10 @@ class usertimeline extends \spouts\spout {
         $result = [];
 
         foreach (self::GROUPED_ENTITY_TYPES as $type) {
+            if (!isset($groupedEntities->{$type})) {
+                continue;
+            }
+
             $entities = $groupedEntities->{$type};
             foreach ($entities as $entity) {
                 $start = $entity->indices[0];