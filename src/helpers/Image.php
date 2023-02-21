fossar/selfoss

commit 44025db9b90ed29663d99b9bb31209210826ffd0

helpers/image: Add SVG support

src/helpers/Image.php
@@ -19,16 +19,23 @@ class Image {
     /** @var ?string url of last fetched favicon */
     private $faviconUrl = null;
 
-    private static $faviconMimeTypes = [
+    private static $imageTypes = [
         // IANA assigned type
-        'image/vnd.microsoft.icon',
+        'image/bmp' => 'bmp',
+        'image/gif' => 'gif',
+        'image/jpeg' => 'jpg',
+        'image/png' => 'png',
+        'image/svg+xml' => 'svg',
+        'image/tiff' => 'tif',
+        'image/vnd.microsoft.icon' => 'ico',
         // Used by Microsoft applications
-        'image/x-icon',
+        'image/x-icon' => 'ico',
+        'image/x-ms-bmp' => 'bmp',
         // Incorrect but sometimes appearing
-        'image/ico',
-        'image/icon',
-        'text/ico',
-        'application/ico'
+        'image/ico' => 'ico',
+        'image/icon' => 'ico',
+        'text/ico' => 'ico',
+        'application/ico' => 'ico',
     ];
 
     private static $iconRelWeights = [
@@ -135,20 +142,51 @@ class Image {
             return null;
         }
 
+        $imgInfo = null;
+
         // get image type
-        $imgInfo = @getimagesizefromstring($data);
-        if (in_array(strtolower($imgInfo['mime']), self::$faviconMimeTypes, true)) {
-            $type = 'ico';
-        } elseif (strtolower($imgInfo['mime']) === 'image/png') {
-            $type = 'png';
-        } elseif (strtolower($imgInfo['mime']) === 'image/jpeg') {
-            $type = 'jpg';
-        } elseif (strtolower($imgInfo['mime']) === 'image/gif') {
-            $type = 'gif';
-        } elseif (strtolower($imgInfo['mime']) === 'image/x-ms-bmp') {
-            $type = 'bmp';
+        if (extension_loaded('imagick')) {
+            // check for svgz or svg
+            if (substr($data, 0, 2) === "\x1f\x8b" && ($d = gzdecode($data) !== false)) {
+                $data = $d;
+            }
+
+            if (preg_match('#<svg[\s>]#si', $data)) {
+                $imgInfo = ['mime' => 'image/svg+xml'];
+            }
+        }
+
+        if ($imgInfo === null) {
+            $imgInfo = @getimagesizefromstring($data);
+        }
+
+        $mimeType = isset($imgInfo['mime']) ? strtolower($imgInfo['mime']) : null;
+        if ($mimeType !== null && isset(self::$imageTypes[$mimeType])) {
+            $type = self::$imageTypes[$mimeType];
         } else {
             return null;
+        }
+
+        // convert svg to png/jpeg
+        if ($type === 'svg') {
+            $image = new \Imagick();
+            $image->readImageBlob($data);
+            if ($width === null || $height === null) {
+                $width = $height = 256;
+            }
+
+            $image->resizeImage($width, $height, \Imagick::FILTER_LANCZOS, 1);
+            if ($extension === 'jpg') {
+                $image->setImageFormat('jpeg');
+                $image->setImageCompression(\Imagick::COMPRESSION_JPEG);
+                $image->setImageCompressionQuality(75);
+            } else {
+                $image->setImageFormat('png24');
+                $image->setImageCompression(\Imagick::COMPRESSION_UNDEFINED);
+                $image->setOption('png:compression-level', 9);
+            }
+
+            return $image;
         }
 
         // convert ico to png

_docs/website/index.html
@@ -93,7 +93,7 @@
             <div class="documentation-entry">
                 <p>selfoss is not a hosted service. It has to be installed on your own webserver. This webserver must fulfil the following requirements (which are available from most providers)</p>
                 <ul>
-                    <li>PHP 5.6 or higher with the php-gd and php-http extensions enabled. Some spouts may also require curl or mbstring extensions.</li>
+                    <li>PHP 5.6 or higher with the php-gd and php-http extensions enabled. Some spouts may also require curl or mbstring extensions. The php-imagick extension is required if you want selfoss to support SVG site icons.</li>
                     <li>MySQL 5.5.3 or higher, PostgreSQL, or SQLite</li>
                     <li>Apache Webserver (nginx and lighttpd also possible)</li>
                 </ul>

commit dcffa85a2ff9dc0b5e0fdbc882ba9de9fd554987

Image: Fix SVGZ
Turns out PHPStan was right in the first place.

This has been broken since it was introduced in https://github.com/fossar/selfoss/commit/44025db9b90ed29663d99b9bb31209210826ffd0

src/helpers/Image.php
@@ -161,8 +161,7 @@ class Image {
         // get image type
         if (extension_loaded('imagick')) {
             // check for svgz or svg
-            if (str_starts_with($data, "\x1f\x8b") && ($d = gzdecode($data) !== false)) {
-                /** @var string $data */ // For PHPStan, it thinks gzdecode returns string|bool rather than string|false.
+            if (str_starts_with($data, "\x1f\x8b") && ($d = gzdecode($data)) !== false) {
                 $data = $d;
             }
 