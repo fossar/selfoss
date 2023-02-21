13871d7cadc4eb1860dd349f62457aa7c0780e43
Jan Tojnar <jtojnar@gmail.com>
2023-02-21 17:40:49 +0100

    helpers/Authentication: Mark as GPL-3.0-or-later
    All contributors have agreed with the relicensing.

--- src/helpers/Authentication.php
+++ src/helpers/Authentication.php
@@ -6,7 +6,7 @@
 // SPDX-FileCopyrightText: 2013 yDelouis <ydelouis@gmail.com>
 // SPDX-FileCopyrightText: 2014–2017 Alexandre Rossi <alexandre.rossi@gmail.com>
 // SPDX-FileCopyrightText: 2016–2023 Jan Tojnar <jtojnar@gmail.com>
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 declare(strict_types=1);
 
