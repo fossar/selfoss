1c969646be989a870637fd0f9cbd5fd8524c64ff
Jan Tojnar <jtojnar@gmail.com>
2023-07-30 14:57:44 +0200

    squash! client: Port to TypeScript
    - Declare `selfoss` inside `window`.

--- client/js/index.ts
+++ client/js/index.ts
@@ -3,5 +3,11 @@ import base from './selfoss-base';
 
 base.init();
 
+declare global {
+  interface Window {
+    selfoss: base;
+  }
+}
+
 // make selfoss available in console for debugging
 window.selfoss = base;
