8643466c205efab3ee87c09777a731eb864037fa
Jan Tojnar <jtojnar@gmail.com>
2024-11-22 23:09:56 +0100

    devenv: Clean up shell.nix
    Naming the components makes it slightly more legible.

--- shell.nix
+++ shell.nix
@@ -1,14 +1,11 @@
-(import
-  (
-    let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
-  )
-  {
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  flake-compat = fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  };
+  self = import flake-compat {
     src = ./.;
-  }
-).shellNix
+  };
+in
+self.shellNix
