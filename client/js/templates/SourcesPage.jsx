fossar/selfoss

commit 88deedea994efe4cdf97207deed9a292057da80a

client: Upgrade to react-router 6
https://remix.run/blog/react-router-v6
https://reactrouter.com/6.28.1/upgrading/v5

- Explicit `History` object manipulation was replaced with `navigate` function.
- Similarly `Redirect` component was replaced with `Navigate` component.
  Also it now defaults to `push` so had to switch to `replace`.
- Regex path had to be replaced with a custom validation logic (see `EntriesFilter` component):
  https://reactrouter.com/6.28.1/start/faq#what-happened-to-regexp-routes-paths
- `Route` `path`s need to be relative.
- `Link`’s `to` prop no longer accepts callback or state.
- `Prompt` component was replaced with an unstable hook.
  Like before, it still does not handle `beforeunload` event.
  https://reactrouter.com/6.28.1/hooks/use-prompt#unstable_useprompt
  But it only works with data routers:
  https://reactrouter.com/6.28.1/routers/picking-a-router
  Yuck, I do not want framework, I just want a routing library.
  Dropping the feature for now.

client/js/templates/SourcesPage.jsx
@@ -1,7 +1,6 @@
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { useMemo } from 'react';
-import { Prompt } from 'react-router';
-import { Link, useHistory, useLocation, useRouteMatch } from 'react-router-dom';
+import { Link, useNavigate, useLocation, useMatch } from 'react-router-dom';
 import Source from './Source';
 import { SpinnerBig } from './Spinner';
 import { LoadingState } from '../requests/LoadingState';
@@ -49,8 +48,8 @@ function handleAddSource({
 // load sources
 function loadSources({
     abortController,
-    history,
     location,
+    navigate,
     setSpouts,
     setSources,
     setLoadingState,
@@ -96,8 +95,10 @@ function loadSources({
                     error instanceof HttpError &&
                     error.response.status === 403
                 ) {
-                    history.push('/sign/in', {
-                        error: selfoss.app._('error_session_expired'),
+                    navigate('/sign/in', {
+                        state: {
+                            error: selfoss.app._('error_session_expired'),
+                        },
                     });
                     return;
                 }
@@ -119,17 +120,17 @@ export default function SourcesPage() {
 
     const forceReload = useShouldReload();
 
-    const history = useHistory();
+    const navigate = useNavigate();
     const location = useLocation();
-    const isAdding = useRouteMatch('/manage/sources/add');
+    const isAdding = useMatch('/manage/sources/add');
 
     useEffect(() => {
         const abortController = new AbortController();
 
         loadSources({
             abortController,
-            history,
             location,
+            navigate,
             setSpouts,
             setSources,
             setLoadingState,
@@ -148,7 +149,7 @@ export default function SourcesPage() {
                 });
 
                 // Clear the value from the state so it does not bug us forever.
-                history.replace('/manage/sources');
+                navigate('/manage/sources');
             }
         });
 
@@ -157,7 +158,7 @@ export default function SourcesPage() {
         };
     }, [
         forceReload,
-        // location.search and history are intentionally omitted
+        // location.search and navigate are intentionally omitted
         // to prevent reloading when the presets are cleaned from the URL.
     ]);
 
@@ -169,10 +170,17 @@ export default function SourcesPage() {
     const _ = useContext(LocalizationContext);
 
     const [dirtySources, setDirtySources] = useState({});
+    // eslint-disable-next-line no-unused-vars
     const isDirty = useMemo(
         () => Object.values(dirtySources).includes(true),
         [dirtySources],
     );
+
+    // TODO: Error: useBlocker must be used within a data router.  See https://reactrouter.com/v6/routers/picking-a-router.
+    // usePrompt({
+    //     when: isDirty,
+    //     message: _('sources_leaving_unsaved_prompt'),
+    // });
 
     if (loadingState === LoadingState.LOADING) {
         return <SpinnerBig label={_('sources_loading')} />;
@@ -184,11 +192,6 @@ export default function SourcesPage() {
 
     return (
         <React.Fragment>
-            <Prompt
-                when={isDirty}
-                message={_('sources_leaving_unsaved_prompt')}
-            />
-
             <button className="source-add" onClick={addOnClick}>
                 {_('source_add')}
             </button>

client/js/helpers/uri.js
@@ -1,4 +1,4 @@
-import { generatePath } from 'react-router-dom';
+import { useLocation, useMatch } from 'react-router-dom';
 import { FilterType } from '../Filter';
 
 /**
@@ -36,9 +36,28 @@ export function filterTypeToString(type) {
         return 'starred';
     }
 }
+function generatePath({ filter, category, id }) {
+    return `/${filter}/${category}${id ? `/${id}` : ''}`;
+}
 
-export const ENTRIES_ROUTE_PATTERN =
-    '/:filter(newest|unread|starred)/:category(all|tag-[^/]+|source-[0-9]+)/:id?';
+export function useEntriesParams() {
+    const match = useMatch(':filter/:category/:id?');
+
+    if (match === null) {
+        return null;
+    }
+
+    const { params } = match;
+    const filterValid = /^(newest|unread|starred)$/.test(params.filter);
+    const categoryValid = /^(all|tag-.+|source-[0-9]+)$/.test(params.category);
+    const idValid = params.id === undefined || /^\d+$/.test(params.id);
+
+    if (!filterValid || !idValid || !categoryValid) {
+        return null;
+    }
+
+    return params;
+}
 
 export function makeEntriesLinkLocation(
     location,
@@ -50,13 +69,13 @@ export function makeEntriesLinkLocation(
     if (location.pathname.match(/^\/(newest|unread|starred)\//) !== null) {
         const [, ...segments] = location.pathname.split('/');
 
-        path = generatePath(ENTRIES_ROUTE_PATTERN, {
+        path = generatePath({
             filter: filter ?? segments[0],
             category: category ?? segments[1],
             id: typeof id !== 'undefined' ? id : segments[2],
         });
     } else {
-        path = generatePath(ENTRIES_ROUTE_PATTERN, {
+        path = generatePath({
             // TODO: change default from config
             filter: filter ?? 'unread',
             category: category ?? 'all',
@@ -91,4 +110,9 @@ export function forceReload(location) {
         ...state,
         forceReload: (state.forceReload ?? 0) + 1,
     };
+}
+
+export function useForceReload() {
+    const location = useLocation();
+    return forceReload(location);
 }

client/js/selfoss-base.js
@@ -281,7 +281,7 @@ const selfoss = {
             await logout();
 
             if (!selfoss.config.publicMode) {
-                selfoss.history.push('/sign/in');
+                selfoss.navigate('/sign/in');
             }
         } catch (error) {
             selfoss.app.showError(

client/js/templates/App.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import nullable from 'prop-types-nullable';
 import {
     BrowserRouter as Router,
-    Switch,
+    Routes,
     Route,
     Link,
-    Redirect,
-    useHistory,
+    Navigate,
+    useNavigate,
     useLocation,
 } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -25,11 +25,11 @@ import * as icons from '../icons';
 import { useAllowedToRead, useAllowedToWrite } from '../helpers/authorizations';
 import { ConfigurationContext } from '../helpers/configuration';
 import { useIsSmartphone, useListenableValue } from '../helpers/hooks';
-import { ENTRIES_ROUTE_PATTERN } from '../helpers/uri';
 import { i18nFormat, LocalizationContext } from '../helpers/i18n';
 import { LoadingState } from '../requests/LoadingState';
 import * as sourceRequests from '../requests/sources';
 import locales from '../locales';
+import { useEntriesParams } from '../helpers/uri';
 
 function handleNavToggle({ event, setNavExpanded }) {
     event.preventDefault();
@@ -91,12 +91,12 @@ function NotFound() {
 }
 
 function CheckAuthorization({ isAllowed, returnLocation, _, children }) {
-    const history = useHistory();
+    const navigate = useNavigate();
     if (!isAllowed) {
         const [preLink, inLink, postLink] = _('error_unauthorized').split(
             /\{(?:link_begin|link_end)\}/,
         );
-        history.push('/sign/in', {
+        navigate('/sign/in', {
             returnLocation,
         });
 
@@ -153,10 +153,10 @@ function PureApp({
     }, []);
 
     // TODO: move stuff that depends on this to the App.
-    const history = useHistory();
+    const navigate = useNavigate();
     useEffect(() => {
-        selfoss.history = history;
-    }, [history]);
+        selfoss.navigate = navigate;
+    }, [navigate]);
 
     // Prepare path of the homepage for redirecting from /
     const homePagePath = configuration.homepage.split('/');
@@ -191,20 +191,20 @@ function PureApp({
         <React.StrictMode>
             <Message message={globalMessage} />
 
-            <Switch>
+            <Routes>
                 <Route
                     path="/sign/in"
-                    render={() => (
+                    element={
                         /* menu open for smartphone */
                         <div id="loginform" role="main">
                             <LoginForm {...{ offlineEnabled }} />
                         </div>
-                    )}
+                    }
                 />
 
                 <Route
                     path="/password"
-                    render={() => (
+                    element={
                         <CheckAuthorization
                             isAllowed={isAllowedToWrite}
                             returnLocation="/password"
@@ -214,12 +214,12 @@ function PureApp({
                                 <HashPassword setTitle={setTitle} />
                             </div>
                         </CheckAuthorization>
-                    )}
+                    }
                 />
 
                 <Route
                     path="/opml"
-                    render={() => (
+                    element={
                         <CheckAuthorization
                             isAllowed={isAllowedToWrite}
                             returnLocation="/opml"
@@ -229,12 +229,12 @@ function PureApp({
                                 <OpmlImport setTitle={setTitle} />
                             </main>
                         </CheckAuthorization>
-                    )}
+                    }
                 />
 
                 <Route
-                    path="/"
-                    render={() => (
+                    path="*"
+                    element={
                         <CheckAuthorization isAllowed={isAllowedToRead} _={_}>
                             <div id="nav-mobile" role="navigation">
                                 <div id="nav-mobile-logo">
@@ -322,21 +322,21 @@ function PureApp({
 
                             {/* content */}
                             <div id="content" role="main">
-                                <Switch>
+                                <Routes>
                                     <Route
-                                        exact
                                         path="/"
-                                        render={() => (
-                                            <Redirect
+                                        element={
+                                            <Navigate
                                                 to={`/${homePagePath.join('/')}`}
+                                                replace
                                             />
-                                        )}
+                                        }
                                     />
                                     <Route
-                                        path={ENTRIES_ROUTE_PATTERN}
-                                        render={() => (
-                                            <EntriesPage
-                                                ref={entriesRef}
+                                        path="/:filter/:category/:id?"
+                                        element={
+                                            <EntriesFilter
+                                                entriesRef={entriesRef}
                                                 setNavExpanded={setNavExpanded}
                                                 configuration={configuration}
                                                 navSourcesExpanded={
@@ -349,22 +349,19 @@ function PureApp({
                                                     setGlobalUnreadCount
                                                 }
                                             />
-                                        )}
+                                        }
                                     />
                                     <Route
-                                        path="/manage/sources"
-                                        render={() => <SourcesPage />}
+                                        path="/manage/sources/add?"
+                                        element={<SourcesPage />}
                                     />
-                                    <Route
-                                        path="*"
-                                        render={() => <NotFound />}
-                                    />
-                                </Switch>
+                                    <Route path="*" element={<NotFound />} />
+                                </Routes>
                             </div>
                         </CheckAuthorization>
-                    )}
+                    }
                 />
-            </Switch>
+            </Routes>
         </React.StrictMode>
     );
 }
@@ -386,6 +383,43 @@ PureApp.propTypes = {
     setSources: PropTypes.func.isRequired,
     tags: PropTypes.arrayOf(PropTypes.object).isRequired,
     reloadAll: PropTypes.func.isRequired,
+};
+
+// Work around for regex patterns not being supported
+// https://github.com/remix-run/react-router/issues/8254
+function EntriesFilter({
+    entriesRef,
+    setNavExpanded,
+    configuration,
+    navSourcesExpanded,
+    unreadItemsCount,
+    setGlobalUnreadCount,
+}) {
+    const params = useEntriesParams();
+
+    if (params === null) {
+        return <NotFound />;
+    }
+
+    return (
+        <EntriesPage
+            ref={entriesRef}
+            setNavExpanded={setNavExpanded}
+            configuration={configuration}
+            navSourcesExpanded={navSourcesExpanded}
+            unreadItemsCount={unreadItemsCount}
+            setGlobalUnreadCount={setGlobalUnreadCount}
+        />
+    );
+}
+
+EntriesFilter.propTypes = {
+    entriesRef: PropTypes.func.isRequired,
+    configuration: PropTypes.object.isRequired,
+    setNavExpanded: PropTypes.func.isRequired,
+    navSourcesExpanded: PropTypes.bool.isRequired,
+    setGlobalUnreadCount: PropTypes.func.isRequired,
+    unreadItemsCount: PropTypes.number.isRequired,
 };
 
 export default class App extends React.Component {
@@ -818,7 +852,15 @@ App.propTypes = {
  */
 export function createApp({ basePath, appRef, configuration }) {
     return (
-        <Router basename={basePath}>
+        <Router
+            basename={basePath}
+            future={{
+                // eslint-disable-next-line camelcase
+                v7_startTransition: true,
+                // eslint-disable-next-line camelcase
+                v7_relativeSplatPath: true,
+            }}
+        >
             <App ref={appRef} configuration={configuration} />
         </Router>
     );

client/js/templates/EntriesPage.jsx
@@ -7,13 +7,7 @@ import React, {
     forwardRef,
 } from 'react';
 import PropTypes from 'prop-types';
-import {
-    Link,
-    useHistory,
-    useLocation,
-    useParams,
-    useRouteMatch,
-} from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import { useOnline } from 'rooks';
 import { useStateWithDeps } from 'use-state-with-deps';
 import nullable from 'prop-types-nullable';
@@ -32,17 +26,14 @@ import { ConfigurationContext } from '../helpers/configuration';
 import { autoScroll, Direction } from '../helpers/navigation';
 import { LocalizationContext } from '../helpers/i18n';
 import { useShouldReload } from '../helpers/hooks';
-import {
-    ENTRIES_ROUTE_PATTERN,
-    forceReload,
-    makeEntriesLinkLocation,
-} from '../helpers/uri';
+import { forceReload, makeEntriesLinkLocation } from '../helpers/uri';
 import { HttpError } from '../errors';
+import { useNavigate } from 'react-router-dom';
 
 function reloadList({
     fetchParams,
     abortController,
-    history,
+    navigate,
     append = false,
     waitForSync = true,
     configuration,
@@ -146,7 +137,7 @@ function reloadList({
                     error instanceof HttpError &&
                     error.response.status === 403
                 ) {
-                    history.push('/sign/in', {
+                    navigate('/sign/in', {
                         error: selfoss.app._('error_session_expired'),
                     });
                     return;
@@ -238,8 +229,8 @@ export function EntriesPage({
     const allowedToWrite = useAllowedToWrite();
     const configuration = useContext(ConfigurationContext);
 
-    const history = useHistory();
     const location = useLocation();
+    const navigate = useNavigate();
     const forceReload = useShouldReload();
     const searchText = useMemo(() => {
         const queryString = new URLSearchParams(location.search);
@@ -306,7 +297,7 @@ export function EntriesPage({
                 fromId,
             },
             abortController,
-            history,
+            navigate,
             append,
             configuration,
             // We do not want to focus the entry on successive loads.
@@ -334,7 +325,7 @@ export function EntriesPage({
         };
     }, [
         configuration,
-        history,
+        navigate,
         params.filter,
         currentTag,
         currentSource,
@@ -769,30 +760,27 @@ class StateHolder extends React.Component {
     }
 
     getActiveTag() {
-        if (!this.props.match) {
+        const category = this.props.params?.category;
+        if (!category) {
             return null;
         }
-        const { params } = this.props.match;
-        return params.category?.startsWith('tag-')
-            ? params.category.replace(/^tag-/, '')
+        return category.startsWith('tag-')
+            ? category.replace(/^tag-/, '')
             : null;
     }
 
     getActiveSource() {
-        if (!this.props.match) {
+        const category = this.props.params?.category;
+        if (!category) {
             return null;
         }
-        const { params } = this.props.match;
-        return params.category?.startsWith('source-')
-            ? parseInt(params.category.replace(/^source-/, ''), 10)
+        return category.startsWith('source-')
+            ? parseInt(category.replace(/^source-/, ''), 10)
             : null;
     }
 
     getActiveFilter() {
-        if (!this.props.match) {
-            return null;
-        }
-        return this.props.match.params.filter;
+        return this.props.params?.filter;
     }
 
     /**
@@ -837,7 +825,7 @@ class StateHolder extends React.Component {
         this.setExpandedEntries({});
         this.props.setNavExpanded(false);
 
-        if (this.props.match.params.filter === FilterType.UNREAD) {
+        if (this.props.params.filter === FilterType.UNREAD) {
             markedEntries = [];
         }
 
@@ -902,8 +890,12 @@ class StateHolder extends React.Component {
                             error instanceof HttpError &&
                             error.response.status === 403
                         ) {
-                            this.props.history.push('/sign/in', {
-                                error: selfoss.app._('error_session_expired'),
+                            this.props.navigate('/sign/in', {
+                                state: {
+                                    error: selfoss.app._(
+                                        'error_session_expired',
+                                    ),
+                                },
                             });
                             return;
                         }
@@ -983,8 +975,12 @@ class StateHolder extends React.Component {
                             error instanceof HttpError &&
                             error.response.status === 403
                         ) {
-                            this.props.history.push('/sign/in', {
-                                error: selfoss.app._('error_session_expired'),
+                            this.props.navigate('/sign/in', {
+                                state: {
+                                    error: selfoss.app._(
+                                        'error_session_expired',
+                                    ),
+                                },
                             });
                             return;
                         }
@@ -1052,8 +1048,12 @@ class StateHolder extends React.Component {
                             error instanceof HttpError &&
                             error.response.status === 403
                         ) {
-                            this.props.history.push('/sign/in', {
-                                error: selfoss.app._('error_session_expired'),
+                            this.props.navigate('/sign/in', {
+                                state: {
+                                    error: selfoss.app._(
+                                        'error_session_expired',
+                                    ),
+                                },
                             });
                             return;
                         }
@@ -1074,11 +1074,14 @@ class StateHolder extends React.Component {
         /**
          * HACK: A counter that is increased every time reload action (r key) is triggered.
          */
-        this.props.history.replace({
-            ...this.props.location,
-            ...makeEntriesLinkLocation(this.props.location, { id: null }),
-            state: forceReload(this.props.location),
-        });
+        this.props.navigate(
+            {
+                ...this.props.location,
+                ...makeEntriesLinkLocation(this.props.location, { id: null }),
+                state: forceReload(this.props.location),
+            },
+            { replace: true },
+        );
     }
 
     /**
@@ -1243,9 +1246,9 @@ class StateHolder extends React.Component {
 
 StateHolder.propTypes = {
     configuration: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
     location: PropTypes.object.isRequired,
-    match: PropTypes.object.isRequired,
+    navigate: PropTypes.func.isRequired,
+    params: PropTypes.object.isRequired,
     setNavExpanded: PropTypes.func.isRequired,
     navSourcesExpanded: PropTypes.bool.isRequired,
     setGlobalUnreadCount: PropTypes.func.isRequired,
@@ -1262,16 +1265,16 @@ const StateHolderOuter = forwardRef(function StateHolderOuter(
     },
     ref,
 ) {
-    const history = useHistory();
     const location = useLocation();
-    const match = useRouteMatch(ENTRIES_ROUTE_PATTERN);
+    const navigate = useNavigate();
+    const params = useParams();
 
     return (
         <StateHolder
             ref={ref}
-            history={history}
             location={location}
-            match={match}
+            navigate={navigate}
+            params={params}
             configuration={configuration}
             setNavExpanded={setNavExpanded}
             navSourcesExpanded={navSourcesExpanded}

client/js/templates/HashPassword.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useCallback, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useInput } from 'rooks';
 import { LoadingState } from '../requests/LoadingState';
 import { HttpError } from '../errors';
@@ -12,7 +12,7 @@ export default function HashPassword({ setTitle }) {
     const [error, setError] = useState(null);
     const passwordEntry = useInput('');
 
-    const history = useHistory();
+    const navigate = useNavigate();
 
     const submit = useCallback(
         (event) => {
@@ -29,7 +29,7 @@ export default function HashPassword({ setTitle }) {
                         error instanceof HttpError &&
                         error.response.status === 403
                     ) {
-                        history.push('/sign/in', {
+                        navigate('/sign/in', {
                             error: 'Generating a new password hash requires being logged in or not setting “password” in selfoss configuration.',
                             returnLocation: '/password',
                         });
@@ -39,7 +39,7 @@ export default function HashPassword({ setTitle }) {
                     setState(LoadingState.ERROR);
                 });
         },
-        [history, passwordEntry.value],
+        [navigate, passwordEntry.value],
     );
 
     useEffect(() => {

commit 7b6af95a4f022b401ffdc5b3d7ffd508af129bc0

client: Fix sources/add → sources redirect
Avoid pushing an extra item to history so that back button works.

This got broken in 88deedea994efe4cdf97207deed9a292057da80a.

client/js/templates/SourcesPage.jsx
@@ -149,7 +149,7 @@ export default function SourcesPage() {
                 });
 
                 // Clear the value from the state so it does not bug us forever.
-                navigate('/manage/sources');
+                navigate('/manage/sources', { replace: true });
             }
         });
 