be4340d54cbd938c3ec74ca3a96ed9c81174161e
Jan Tojnar <jtojnar@gmail.com>
2025-10-03 23:06:19 +0200

    Configuration: Use `Uri` type for `base_url`
    More type safe and allows to introduce extra validations.

--- src/controllers/Rss.php
+++ src/controllers/Rss.php
@@ -44,7 +44,7 @@ class Rss {
         $this->feedWriter->setChannelElement('description', '');
         $this->feedWriter->setSelfLink($this->view->getBaseUrl() . 'feed');
 
-        $this->feedWriter->setLink($this->view->getBaseUrl());
+        $this->feedWriter->setLink((string) $this->view->getBaseUrl());
 
         // get sources
         $lastSourceId = 0;
