b8a0f9d8a51845eb0720eb05371c22c0befcd262
Jan Tojnar <jtojnar@gmail.com>
2025-10-04 05:06:18 +0200

    helpers\View: Reduce nesting in `makeBaseUrl`
    Use early return to make the code slightly more legible.
    
    This is also a prerequisite for overriding just the path when non-absolute URL is configured.

--- src/helpers/View.php
+++ src/helpers/View.php
@@ -46,43 +46,47 @@ final class View {
     }
 
     private function makeBaseUrl(): Uri {
-        // base url in config.ini file
         $base = $this->configuration->baseUrl;
-        if ($base === null) {
-            // auto generate base url
-            $protocol = 'http';
-            if ((isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
-                || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
-                || (isset($_SERVER['HTTP_HTTPS']) && $_SERVER['HTTP_HTTPS'] === 'https')) {
-                $protocol = 'https';
-            }
 
-            // check for SSL proxy
-            if (isset($_SERVER['HTTP_X_FORWARDED_SERVER']) && isset($_SERVER['HTTP_X_FORWARDED_HOST'])
-            && ($_SERVER['HTTP_X_FORWARDED_SERVER'] === $_SERVER['HTTP_X_FORWARDED_HOST'])) {
-                $subdir = '/' . preg_replace('/\/[^\/]+$/', '', $_SERVER['PHP_SELF']);
-                $host = $_SERVER['HTTP_X_FORWARDED_SERVER'];
-            } else {
-                $subdir = '';
-                if (PHP_SAPI !== 'cli') {
-                    $subdir = rtrim(strtr(dirname($_SERVER['SCRIPT_NAME']), '\\', '/'), '/');
-                }
-                $host = $_SERVER['SERVER_NAME'];
-            }
-
-            $port = '';
-            if (isset($_SERVER['SERVER_PORT'])
-                && (($protocol === 'http' && $_SERVER['SERVER_PORT'] != '80')
-                    || ($protocol === 'https' && $_SERVER['SERVER_PORT'] != '443'))) {
-                $port = ':' . $_SERVER['SERVER_PORT'];
-            }
-            // Override the port if nginx is the front end and the traffic is being forwarded
-            if (isset($_SERVER['HTTP_X_FORWARDED_PORT'])) {
-                $port = ':' . $_SERVER['HTTP_X_FORWARDED_PORT'];
-            }
-
-            $base = new Uri($protocol . '://' . $host . $port . $subdir . '/');
+        if ($base !== null) {
+            // base url in config.ini file
+            return $base;
         }
+
+        // auto generate base url
+
+        $protocol = 'http';
+        if ((isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+            || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+            || (isset($_SERVER['HTTP_HTTPS']) && $_SERVER['HTTP_HTTPS'] === 'https')) {
+            $protocol = 'https';
+        }
+
+        // check for SSL proxy
+        if (isset($_SERVER['HTTP_X_FORWARDED_SERVER']) && isset($_SERVER['HTTP_X_FORWARDED_HOST'])
+        && ($_SERVER['HTTP_X_FORWARDED_SERVER'] === $_SERVER['HTTP_X_FORWARDED_HOST'])) {
+            $subdir = '/' . preg_replace('/\/[^\/]+$/', '', $_SERVER['PHP_SELF']);
+            $host = $_SERVER['HTTP_X_FORWARDED_SERVER'];
+        } else {
+            $subdir = '';
+            if (PHP_SAPI !== 'cli') {
+                $subdir = rtrim(strtr(dirname($_SERVER['SCRIPT_NAME']), '\\', '/'), '/');
+            }
+            $host = $_SERVER['SERVER_NAME'];
+        }
+
+        $port = '';
+        if (isset($_SERVER['SERVER_PORT'])
+            && (($protocol === 'http' && $_SERVER['SERVER_PORT'] != '80')
+                || ($protocol === 'https' && $_SERVER['SERVER_PORT'] != '443'))) {
+            $port = ':' . $_SERVER['SERVER_PORT'];
+        }
+        // Override the port if nginx is the front end and the traffic is being forwarded
+        if (isset($_SERVER['HTTP_X_FORWARDED_PORT'])) {
+            $port = ':' . $_SERVER['HTTP_X_FORWARDED_PORT'];
+        }
+
+        $base = new Uri($protocol . '://' . $host . $port . $subdir . '/');
 
         return $base;
     }
