424fc559be91c2e5fa72d89454c8feca72d2daa6
Jan Tojnar <jtojnar@gmail.com>
2025-12-23 02:11:53 +0100

    Add rector config
    This allows for various source code transformations.
    
    Just run `composer rector` to apply.

---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-FileCopyrightText: 2025 Jan Tojnar <jtojnar@gmail.com>
+ */
+
+use Rector\Config\RectorConfig;
+use Rector\Php73\Rector\String_\SensitiveHereNowDocRector;
+
+return RectorConfig::configure()
+    ->withRootFiles()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets()
+    ->withPreparedSets(
+        typeDeclarations: true,
+    )
+    ->withSkip([
+        // Case-insensitivity leads to false positives.
+        SensitiveHereNowDocRector::class,
+    ])
+;
