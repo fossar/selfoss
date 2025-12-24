fossar/selfoss

commit 1041a6cae586ae1388734355789b47c2d8536c45

client: Add more type annotations
Slowly fixing `noImplicitAny` for TypeScript compiler.

client/js/templates/EntriesPage.tsx
@@ -77,7 +77,7 @@ function reloadList({
 
     setLoadingState(LoadingState.LOADING);
 
-    const reload = () => {
+    const reload = (): Promise<void> => {
         if (abortController.signal.aborted) {
             return Promise.resolve();
         }
@@ -181,9 +181,8 @@ function reloadList({
 
 /**
  * Try to open the selected article using the preferred method.
- * @param {number} selected
  */
-function openSelectedArticle(selected) {
+function openSelectedArticle(selected: number): void {
     const link = document.querySelector<HTMLDivElement>(
         `.entry[data-entry-id="${selected}"] .entry-datetime`,
     );
@@ -209,7 +208,13 @@ function handleRefreshSource({
     setLoadingState,
     setNavExpanded,
     reload,
-}) {
+}: {
+    event: MouseEvent<HTMLButtonElement>;
+    source: number;
+    setLoadingState: Dispatch<SetStateAction<LoadingState>>;
+    setNavExpanded: Dispatch<SetStateAction<boolean>>;
+    reload: () => void;
+}): Promise<void> {
     event.preventDefault();
 
     // show loading
@@ -247,7 +252,7 @@ type EntriesPageProps = {
     unreadItemsCount: number;
 };
 
-export function EntriesPage(props: EntriesPageProps) {
+export function EntriesPage(props: EntriesPageProps): React.JSX.Element {
     const {
         entries,
         hasMore,
@@ -355,7 +360,7 @@ export function EntriesPage(props: EntriesPageProps) {
     useEffect(() => {
         const abortController = new AbortController();
 
-        const fetchParams = {
+        const fetchParams: FetchParams = {
             type: params.filter,
             tag: currentTag,
             source: currentSource,
@@ -563,7 +568,7 @@ export function EntriesPage(props: EntriesPageProps) {
     );
 }
 
-const initialState = {
+const initialState: StateHolderState = {
     entries: [],
     hasMore: false,
     selectedEntry: null,
@@ -629,7 +634,7 @@ export class StateHolder extends React.Component<
         this.throw = this.throw.bind(this);
     }
 
-    setEntries(entries) {
+    setEntries(entries: SetStateAction<ResponseItem[]>): void {
         if (typeof entries === 'function') {
             this.setState((state) => ({
                 entries: entries(state.entries),
@@ -639,7 +644,7 @@ export class StateHolder extends React.Component<
         }
     }
 
-    appendEntries(extraEntries) {
+    appendEntries(extraEntries: ResponseItem[]): void {
         this.setEntries((entries) => [...entries, ...extraEntries]);
     }
 
@@ -777,7 +782,7 @@ export class StateHolder extends React.Component<
         );
     }
 
-    refreshEntryStatuses(entryStatuses: EntryStatus[]) {
+    refreshEntryStatuses(entryStatuses: EntryStatus[]): void {
         this.state.entries.forEach((entry) => {
             const { id } = entry;
             const newStatus = entryStatuses.find(
@@ -884,7 +889,7 @@ export class StateHolder extends React.Component<
         this.setEntries(markedEntries);
 
         // update statistics in main menu
-        const updateStats = (markRead) => {
+        const updateStats = (markRead: boolean) => {
             // update all unread counters
             const unreadstats = selfoss.app.state.unreadItemsCount;
             const diff = markRead ? -1 : 1;
@@ -980,7 +985,7 @@ export class StateHolder extends React.Component<
         this.markEntryInView(id, !newReadState);
 
         // update statistics in main menue and the currently active tag
-        const updateStats = (markRead) => {
+        const updateStats = (markRead: boolean) => {
             // update all unread counters
             const unreadstats = selfoss.app.state.unreadItemsCount;
             const diff = markRead ? -1 : 1;
@@ -1065,7 +1070,7 @@ export class StateHolder extends React.Component<
         this.starEntryInView(id, newStarredState);
 
         // update statistics in main menu
-        const updateStats = (markStarred) => {
+        const updateStats = (markStarred: boolean) => {
             selfoss.app.setStarredItemsCount(
                 (starred) => starred + (markStarred ? 1 : -1),
             );
@@ -1274,7 +1279,7 @@ export class StateHolder extends React.Component<
         this.nextPrev(direction, true);
     }
 
-    render() {
+    render(): React.JSX.Element {
         return (
             <EntriesPage
                 entries={this.state.entries}
@@ -1302,7 +1307,9 @@ type StateHolderOuterProps = {
     ref: ForwardedRef<StateHolder>;
 };
 
-export function StateHolderOuter(props: StateHolderOuterProps) {
+export function StateHolderOuter(
+    props: StateHolderOuterProps,
+): React.JSX.Element {
     const {
         configuration,
         setNavExpanded,

client/js/helpers/ValueListenable.ts
@@ -19,7 +19,7 @@ export class ValueListenable<T> extends EventTarget {
         this.value = value;
     }
 
-    update(value: T) {
+    update(value: T): void {
         if (this.value !== value) {
             this.value = value;
 

client/js/selfoss-base.ts
@@ -7,6 +7,7 @@ import { HttpError, TimeoutError } from './errors';
 import { Configuration } from './model/Configuration';
 import { LoadingState } from './requests/LoadingState';
 import { App, createApp } from './templates/App';
+import { StateHolder as EntriesPageStateful } from './templates/EntriesPage';
 import DbOnline from './selfoss-db-online';
 import DbOffline from './selfoss-db-offline';
 import Db from './selfoss-db';
@@ -29,7 +30,7 @@ class selfoss {
     /**
      * React component for entries page.
      */
-    public static entriesPage = null;
+    public static entriesPage: EntriesPageStateful | null = null;
 
     private static serviceWorkerInitialized = false;
 

client/js/selfoss-db-offline.ts
@@ -59,7 +59,7 @@ export default class DbOffline {
             });
     }
 
-    init() {
+    init(): PromiseExtended<void> {
         if (!selfoss.db.enableOffline.value || selfoss.db.storage) {
             return;
         }
@@ -159,7 +159,7 @@ export default class DbOffline {
             });
     }
 
-    _memLastItemId() {
+    _memLastItemId(): PromiseExtended<void> {
         return selfoss.db.storage.entries
             .orderBy('id')
             .reverse()
@@ -172,7 +172,7 @@ export default class DbOffline {
             });
     }
 
-    storeEntries(entries) {
+    storeEntries(entries: readonly ResponseItem[]): PromiseExtended<void> {
         return this._tr(
             'rw',
             [selfoss.db.storage.entries, selfoss.db.storage.stamps],
@@ -188,7 +188,7 @@ export default class DbOffline {
         );
     }
 
-    GCEntries(more = false) {
+    GCEntries(more: boolean = false): PromiseExtended<void> {
         if (more) {
             // We need to garbage collect more, as the browser storage limit
             // seems to be exceeded: decrease the amount of days entries are
@@ -366,7 +366,7 @@ export default class DbOffline {
             .then((entries) => ({ entries, hasMore }));
     }
 
-    reloadOnlineStats() {
+    reloadOnlineStats(): PromiseExtended<void> {
         return this._tr('r', [selfoss.db.storage.stats], () => {
             selfoss.db.storage.stats.toArray((stats) => {
                 const newStats = {
@@ -386,7 +386,7 @@ export default class DbOffline {
         });
     }
 
-    refreshStats() {
+    refreshStats(): PromiseExtended<void> {
         return this._tr('r', [selfoss.db.storage.entries], () => {
             const offlineCounts = { newest: 0, unread: 0, starred: 0 };
 
@@ -428,7 +428,11 @@ export default class DbOffline {
         });
     }
 
-    enqueueStatus(entryId, statusName, statusValue) {
+    enqueueStatus(
+        entryId: number,
+        statusName: string,
+        statusValue: boolean,
+    ): Promise<void> {
         return this.enqueueStatuses([
             {
                 entryId,
@@ -438,7 +442,7 @@ export default class DbOffline {
         ]);
     }
 
-    sendNewStatuses() {
+    sendNewStatuses(): Promise<void> {
         selfoss.db.storage.statusq
             .toArray()
             .then((statuses) => {

client/js/selfoss-db-online.ts
@@ -38,7 +38,7 @@ export default class DbOnline {
     public statsDirty: boolean = false;
     public firstSync: boolean = true;
 
-    _syncBegin() {
+    _syncBegin(): Promise<void> {
         if (!this.syncing.promise) {
             this.syncing.promise = new Promise((resolve, reject) => {
                 this.syncing.resolve = resolve;

client/js/selfoss-db.ts
@@ -31,7 +31,7 @@ export default class Db {
 
     public lastSync: number | null = null;
 
-    setOnline() {
+    setOnline(): void {
         if (!this.online) {
             this.online = true;
             this.sync();
@@ -57,7 +57,7 @@ export default class Db {
         }
     }
 
-    clear() {
+    clear(): Promise<void> {
         if (this.storage) {
             window.localStorage.removeItem('offlineDays');
             const clearing = this.storage.delete();
@@ -69,14 +69,14 @@ export default class Db {
         }
     }
 
-    isValidTag(name) {
+    isValidTag(name: string): boolean {
         return (
             selfoss.app.state.tags.length === 0 ||
             selfoss.app.state.tags.find((tag) => tag.tag === name) !== undefined
         );
     }
 
-    isValidSource(id) {
+    isValidSource(id: number): boolean {
         return (
             selfoss.app.state.sources.length === 0 ||
             selfoss.app.state.sources.find((source) => source.id === id) !==
@@ -84,7 +84,7 @@ export default class Db {
         );
     }
 
-    sync(force = false) {
+    sync(force = false): Promise<void> {
         const lastUpdateIsOld =
             this.lastUpdate === null ||
             this.lastSync === null ||

client/js/templates/App.tsx
@@ -7,6 +7,7 @@ import React, {
     useEffectEvent,
     useMemo,
     useState,
+    MouseEvent,
 } from 'react';
 import {
     BrowserRouter as Router,
@@ -51,7 +52,7 @@ type GlobalMessage = {
 };
 
 function handleNavToggle(args: {
-    event: Event;
+    event: MouseEvent<HTMLButtonElement>;
     setNavExpanded: Dispatch<SetStateAction<boolean>>;
 }): void {
     const { event, setNavExpanded } = args;
@@ -167,7 +168,7 @@ type EntriesFilterProps = {
 
 // Work around for regex patterns not being supported
 // https://github.com/remix-run/react-router/issues/8254
-function EntriesFilter(props: EntriesFilterProps) {
+function EntriesFilter(props: EntriesFilterProps): React.JSX.Element {
     const {
         entriesRef,
         setNavExpanded,
@@ -262,11 +263,12 @@ function PureApp(props: PureAppProps): React.JSX.Element {
     }
 
     const menuButtonOnClick = useCallback(
-        (event) => handleNavToggle({ event, setNavExpanded }),
+        (event: MouseEvent<HTMLButtonElement>) =>
+            handleNavToggle({ event, setNavExpanded }),
         [],
     );
 
-    const entriesRef = useCallback((entriesPage) => {
+    const entriesRef = useCallback((entriesPage: EntriesPageStateful) => {
         setEntriesPage(entriesPage);
         selfoss.entriesPage = entriesPage;
 

client/js/templates/ColorChooser.tsx
@@ -1,4 +1,4 @@
-import React, { use, useMemo } from 'react';
+import React, { MouseEvent, use, useMemo } from 'react';
 import { ClickEvent, Menu, MenuButton, MenuItem } from '@szhsin/react-menu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { colorByBrightness } from '../helpers/color';
@@ -76,7 +76,7 @@ type ColorButtonProps = {
     color: string;
 };
 
-function ColorButton(props: ColorButtonProps) {
+function ColorButton(props: ColorButtonProps): React.JSX.Element {
     const { tag, color } = props;
 
     const style = useMemo(
@@ -96,7 +96,7 @@ function ColorButton(props: ColorButtonProps) {
     );
 }
 
-const preventDefault = (event) => {
+const preventDefault = (event: MouseEvent<HTMLDivElement>): void => {
     event.preventDefault();
     // Prevent closing navigation on mobile.
     event.stopPropagation();
@@ -107,7 +107,9 @@ type ColorChooserProps = {
     onChange: (event: ClickEvent) => void;
 };
 
-export default function ColorChooser(props: ColorChooserProps) {
+export default function ColorChooser(
+    props: ColorChooserProps,
+): React.JSX.Element {
     const { tag, onChange } = props;
 
     const style = useMemo(() => ({ backgroundColor: tag.color }), [tag.color]);

commit 52bb4c88185c01fdeef8cb2f161370b9042be9b6

client/EntriesPage: Fix regression in open action
    TypeError: eD.MouseEvent is not a constructor

I accidentally broke it in 1041a6cae586ae1388734355789b47c2d8536c45.

client/js/templates/EntriesPage.tsx
@@ -7,7 +7,6 @@ import React, {
     Dispatch,
     SetStateAction,
     ForwardedRef,
-    MouseEvent,
     useEffectEvent,
 } from 'react';
 import { Link, NavigateFunction } from 'react-router';
@@ -213,7 +212,7 @@ function handleRefreshSource({
     reload,
     _,
 }: {
-    event: MouseEvent<HTMLButtonElement>;
+    event: React.MouseEvent<HTMLButtonElement>;
     source: number;
     setLoadingState: Dispatch<SetStateAction<LoadingState>>;
     setNavExpanded: Dispatch<SetStateAction<boolean>>;
@@ -448,7 +447,7 @@ export function EntriesPage(props: EntriesPageProps): React.JSX.Element {
     const isOnline = useOnline();
 
     const refreshOnClick = useCallback(
-        (event: MouseEvent<HTMLButtonElement>) =>
+        (event: React.MouseEvent<HTMLButtonElement>) =>
             handleRefreshSource({
                 event,
                 source: currentSource,
@@ -461,7 +460,7 @@ export function EntriesPage(props: EntriesPageProps): React.JSX.Element {
     );
 
     const moreOnClick = useCallback(
-        (event: MouseEvent<HTMLButtonElement>) => {
+        (event: React.MouseEvent<HTMLButtonElement>) => {
             event.preventDefault();
             const lastEntry = entries[entries.length - 1];
 